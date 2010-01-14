a561f8a88792647076baa0414169ec42d0f39aad
Chuck Fry <chuck.fry@nasa.gov>
2010-01-14 00:48:50 +0000

    Incorporate contributions from Cesar's team at Langley
    git-svn-id: https://svn.code.sf.net/p/plexil/code/trunk@922 d2be67b0-814c-0410-949f-d215b6b29573

--- src/luv/src/gov/nasa/luv/ExecutionHandler.java
+++ src/luv/src/gov/nasa/luv/ExecutionHandler.java
@@ -25,11 +25,27 @@
 */
 
 package gov.nasa.luv;
-import java.io.*;
-import static gov.nasa.luv.Constants.*;
+import static gov.nasa.luv.Constants.DEBUG_CFG_FILE;
+import static gov.nasa.luv.Constants.TEST_EXEC;
+import static gov.nasa.luv.Constants.UE_EXEC;
+import static gov.nasa.luv.Constants.UNKNOWN;
+import gov.nasa.luv.runtime.AbstractPlexilExecutiveCommandGenerator;
+import gov.nasa.luv.runtime.ExecutiveCommandGenerationException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /** The ExecutionHandler class runs an instance of the Universal Executive. */
 
+/**
+ * Adapted by Hector Fabio Cadavid Rengifo. hector.cadavid@escuelaing.edu.co
+ */
 public class ExecutionHandler
 {
       private ExecutionHandler ee;   
@@ -102,96 +118,145 @@ public class ExecutionHandler
           return false;
       }
       
-    /** Creates the command to execute the Universal Executive.
-     * 
-     *  @return the command to execute the Universal Executive or an error message if the command could not be created.
-     */
+      /**
+       * This methods returns a concrete PlexilExecutiveCommandGenerator. By default, this method returns a standard UE command
+       * generator. If a value for "ALT_EXECUTIVE" system variable is given when LUV is started, a different executive command
+       * generator will be used (will be used the class name given on such variable).
+       * @return a plexil command generator.
+       */
+      private AbstractPlexilExecutiveCommandGenerator getPlexilExecutive() throws ExecutiveCommandGenerationException{
+    	  String alternativeExecutive=System.getProperty("ALT_EXECUTIVE");
+    	  
+    	  if (alternativeExecutive==null){
+    		  return new PlexilUniversalExecutive();
+    	  }
+    	  else{
+    		  try {    			
+				Class ecgClass=Class.forName(alternativeExecutive);
+				Constructor ecgClCons=ecgClass.getConstructor(new Class[]{});
+				Object o=ecgClCons.newInstance(new Object[]{});
+				if (o instanceof AbstractPlexilExecutiveCommandGenerator){
+					return (AbstractPlexilExecutiveCommandGenerator)o;
+				}
+				else{
+					throw new ExecutiveCommandGenerationException("The class given with the ALT_EXECUTIVE system variable:"+alternativeExecutive+", must be an AbstractPlexilExecutiveCommandGenerator subclass.");
+				}
+			} catch (ClassNotFoundException e) {
+				throw new ExecutiveCommandGenerationException("The class given with the ALT_EXECUTIVE system variable:"+alternativeExecutive+", doesn't exist.");
+			} catch (SecurityException e) {
+				throw new ExecutiveCommandGenerationException("Error when trying to create an instance of the given ALT_EXECUTIVE system variable:"+alternativeExecutive+".");
+			} catch (NoSuchMethodException e) {
+				throw new ExecutiveCommandGenerationException("Error when trying to create an instance of the given ALT_EXECUTIVE system variable:"+alternativeExecutive+".");
+			} catch (IllegalArgumentException e) {
+				throw new ExecutiveCommandGenerationException("Error when trying to create an instance of the given ALT_EXECUTIVE system variable:"+alternativeExecutive+".");
+			} catch (InstantiationException e) {
+				throw new ExecutiveCommandGenerationException("Error when trying to create an instance of the given ALT_EXECUTIVE system variable:"+alternativeExecutive+".");
+			} catch (IllegalAccessException e) {
+				throw new ExecutiveCommandGenerationException("Error when trying to create an instance of the given ALT_EXECUTIVE system variable:"+alternativeExecutive+".");
+			} catch (InvocationTargetException e) {
+				throw new ExecutiveCommandGenerationException("Error when trying to create an instance of the given ALT_EXECUTIVE system variable:"+alternativeExecutive+".");
+			}
+    	  }    	  
+      }
       
+      
+      
+      /** Creates the command to execute the Universal Executive.
+       * 
+       *  @return the command to execute the Universal Executive or an error message if the command could not be created.
+       */      
       private String createCommandLine() throws IOException
       {
-          Model currentPlan = Luv.getLuv().getCurrentPlan();
-          String command = UE_EXEC + " -v";
+    	  AbstractPlexilExecutiveCommandGenerator pe;
+    	  try {
+    		  pe = getPlexilExecutive();
+    	  } catch (ExecutiveCommandGenerationException e) {
+    		  return "ERROR: unable to load alternative plexil executive:"+e.getMessage();	
+    	  }
 
-	  if (Luv.getLuv().breaksAllowed())
-	      command += " -b"; 
-          
-          command += " -d " + DEBUG_CFG_FILE;
-  
-          // get plan
-        
-	  if (currentPlan != null && 
-              currentPlan.getAbsolutePlanName() != null &&
-              !currentPlan.getAbsolutePlanName().equals(UNKNOWN))
-          {
-              if (new File(currentPlan.getAbsolutePlanName()).exists())
-              {
-                  command += " " + currentPlan.getAbsolutePlanName(); 
-              }
-              else
-                  return "ERROR: unable to identify plan.";
-          }
-          else
-              return "ERROR: unable to identify plan.";
-        
-          // get script
-        
-          if (currentPlan != null &&
-              currentPlan.getAbsoluteScriptName() != null &&
-              !currentPlan.getAbsoluteScriptName().equals(UNKNOWN))
-          {
-              if (new File(currentPlan.getAbsoluteScriptName()).exists())
-              {
-                  command += " " + currentPlan.getAbsoluteScriptName(); 
-              }
-              else if (Luv.getLuv().getFileHandler().searchForScript() != null)
-              {
-                  command += " " + currentPlan.getAbsoluteScriptName();
-              }
-              else
-                  return "ERROR: unable to identify script.";
-          }
-          else if (Luv.getLuv().getFileHandler().searchForScript() != null)
-          {
-              command += " " + currentPlan.getAbsoluteScriptName();
-          }
-          else
-              return "ERROR: unable to identify script.";
-             
-          // get libraries
+    	  Model currentPlan = Luv.getLuv().getCurrentPlan();
 
-          if (!currentPlan.getMissingLibraries().isEmpty()) {
-              // try to find libraries
-              for (String libName : currentPlan.getMissingLibraries()) {
-                  Model lib = Luv.getLuv().getCurrentPlan().findLibraryNode(libName, true);
-                  if (lib == null) 
-                  {
-                      return "ERROR: library \"" + libName + "\" not found.";
-                  }
-                  else 
-                  {
-		      currentPlan.linkLibrary(lib);
-                  }
-              }
-          }
+    	  pe.setCurrentPlan(currentPlan);
 
-          if (!currentPlan.getLibraryNames().isEmpty()) 
-          {
-              for (String libFile : currentPlan.getLibraryNames()) 
-              {
-                  // double check that library still exists
-                  if (new File(libFile).exists()) 
-                  {
-                      command += " -l ";
-                      command += libFile;
-                  }
-                  else 
-                  {
-                      return "ERROR: library file " + libFile + " does not exist.";
-                  }
-              }
-          }       
 
-	  return command;
+    	  if (currentPlan != null && 
+    			  currentPlan.getAbsolutePlanName() != null &&
+    			  !currentPlan.getAbsolutePlanName().equals(UNKNOWN))
+    	  {
+    		  if (!new File(currentPlan.getAbsolutePlanName()).exists())
+    		  {
+    			  return "ERROR: unable to identify plan."; 
+    		  }
+
+    	  }
+    	  else
+    		  return "ERROR: unable to identify plan.";
+
+
+    	  // get script
+
+    	  if (currentPlan != null &&
+    			  currentPlan.getAbsoluteScriptName() != null &&
+    			  !currentPlan.getAbsoluteScriptName().equals(UNKNOWN))
+    	  {
+    		  if (new File(currentPlan.getAbsoluteScriptName()).exists())
+    		  {
+    			  pe.setScriptPath(currentPlan.getAbsoluteScriptName()); 
+    		  }
+    		  else if (Luv.getLuv().getFileHandler().searchForScript() != null)
+    		  {
+    			  pe.setScriptPath(currentPlan.getAbsoluteScriptName());
+    		  }
+    		  else
+    			  return "ERROR: unable to identify script.";
+    	  }
+    	  else if (Luv.getLuv().getFileHandler().searchForScript() != null)
+    	  {
+    		  pe.setScriptPath(currentPlan.getAbsoluteScriptName());
+    	  }
+    	  else
+    		  return "ERROR: unable to identify script.";
+
+    	  // get libraries
+
+    	  if (!currentPlan.getMissingLibraries().isEmpty()) {
+    		  // try to find libraries
+    		  for (String libName : currentPlan.getMissingLibraries()) {
+    			  Model lib = Luv.getLuv().getCurrentPlan().findLibraryNode(libName, true);
+    			  if (lib == null) 
+    			  {
+    				  return "ERROR: library \"" + libName + "\" not found.";
+    			  }
+    			  else 
+    			  {
+    				  currentPlan.linkLibrary(lib);
+    			  }
+    		  }
+    	  }
+
+    	  if (!currentPlan.getLibraryNames().isEmpty()) 
+    	  {
+    		  Set<String> libFiles=new LinkedHashSet<String>();
+    		  for (String libFile : currentPlan.getLibraryNames()) 
+    		  {
+    			  // double check that library still exists
+    			  if (new File(libFile).exists()) 
+    			  {
+    				  libFiles.add(libFile);
+    			  }
+    			  else 
+    			  {
+    				  return "ERROR: library file " + libFile + " does not exist.";
+    			  }
+    		  }
+    		  pe.setLibFiles(libFiles);
+    	  }       
+
+    	  try {
+    		  return pe.generateCommandLine();
+    	  } catch (ExecutiveCommandGenerationException e) {
+    		  return "ERROR: unable to create executive command line:"+e.getMessage();
+    	  }
       }
       
     /** Kills the currently running instance of the Universal Executive. */
@@ -234,4 +299,42 @@ public class ExecutionHandler
               System.out.println(line);
           }
       }
+}
+
+
+
+class PlexilUniversalExecutive extends AbstractPlexilExecutiveCommandGenerator{
+
+	@Override
+	public String generateCommandLine() {
+	  System.out.println("Using Universal Executive...");
+		
+	  String command = UE_EXEC + " -v";
+
+	  if (this.isBreaksAllowed()){
+		  command += " -b";
+	  }
+		   
+	  command += " -d " + DEBUG_CFG_FILE;
+
+	  // get plan
+
+	  Model currentPlan=this.getCurrentPlan();
+	  
+	  command += " " + currentPlan.getAbsolutePlanName(); 
+
+	  
+	  command += " " + this.getScriptPath(); 
+
+	  if (this.getLibFiles()!=null){
+		  for (String lf:this.getLibFiles()){
+			  command += " -l ";
+			  command += lf;		  			  
+		  }
+	  }
+
+	  return command;
+
+	}
+
 }
---
+++ src/luv/src/gov/nasa/luv/runtime/ExecutiveCommandGenerationException.java
@@ -0,0 +1,20 @@
+package gov.nasa.luv.runtime;
+
+public class ExecutiveCommandGenerationException extends Exception {
+
+	public ExecutiveCommandGenerationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExecutiveCommandGenerationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExecutiveCommandGenerationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
