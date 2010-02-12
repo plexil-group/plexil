8e4ae1346303f5af27217c4879bc422b35404e21
Chuck Fry <chuck.fry@nasa.gov>
2010-02-12 22:29:45 +0000

    Broke out KeySource template class in preparation for other changes
    git-svn-id: https://svn.code.sf.net/p/plexil/code/trunk@1031 d2be67b0-814c-0410-949f-d215b6b29573

--- src/utils/StoredItem.hh
+++ src/utils/StoredItem.hh
@@ -35,248 +35,23 @@
  * @ingroup Utils
  */
 
-#include "Error.hh"
-#include "ThreadMutex.hh"
-#include <limits>
-#include <map>
-#include <stack>
 #include <ext/hash_map>
-#include <ext/hash_set>
 #if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ > 3)
 #include <ext/hash_fun.h>
 #else
 #include <ext/stl_hash_fun.h>
 #endif
 
-// define STORED_ITEM_REUSE_KEYS to allow key reuse, which should help in
-// cases where key exhaustion occurs
+// Define STORED_ITEM_REUSE_KEYS to allow key reuse, which should help in
+// cases where key exhaustion occurs.
+// If defined, must come before including KeySource.hh, as that file depends on it.
 
 //#define STORED_ITEM_REUSE_KEYS
 
+#include "KeySource.hh"
 
 namespace PLEXIL
 {
-   template <class key_t, 
-             class item_t, 
-             class itemHashFunction_t, 
-             class itemEqualFunction_t,
-             class keyHashFunction_t,
-             class keyEqualFunction_t>
-   class StoredItem;
-
-
-   /**
-    * @class KeySource
-    * @brief Provides an abstract key source for StoredItem.
-    *
-    * Floating point keys use the the denormalized range of values for
-    * keys.  Integer types use the full range of values for that type.
-    */
-
-   template <class key_t>
-   class KeySource
-   {
-         // numeric limits of key type
-
-         typedef typename std::numeric_limits<key_t>       keyLimits_t;
-
-#ifdef STORED_ITEM_REUSE_KEYS
-         // key pool type
-
-         typedef typename std::stack<key_t>                keyPool_t;
-#endif
-
-      public:
-
-         /**
-          * @brief Returns the next available key.
-          */
-
-         inline static const key_t next()
-         {
-            // make key generation thread safe
-
-            ThreadMutexGuard guard(getMutex());
-
-            key_t sl_key;
-
-#ifdef STORED_ITEM_REUSE_KEYS
-
-            // if the key pool is not empty, use a key from there
-
-            if (!keyPool().empty())
-            {
-               sl_key = keyPool().top();
-               keyPool().pop();
-            }
-            
-            // otherwise increment the counter
-            
-            else
-#endif
-            {
-               assertTrue(availableKeys() > 0, "Key space exhausted.");
-               sl_key = counter();
-               counter() += increment();
-            }
-
-            // get next key value 
-
-            return sl_key;
-         }
-
-         /**
-          * @brief Returns the total number of keys which may be
-          * generated.
-          */
-
-         static const size_t totalKeys()
-         {
-            return ((size_t)((max() - min()) / increment())) - 1;
-         }
-
-         /**
-          * @brief Returns the remaing number of keys which are
-          * available.
-          */
-
-         static const size_t availableKeys()
-         {
-            return (totalKeys() 
-                    - (size_t)((counter() - min()) / increment()))
-#ifdef STORED_ITEM_REUSE_KEYS
-               + keyPool().size()
-#endif
-               ;
-         }
-
-         /**
-          * @brief Free key for possible reassignment.
-          *
-          * If STORED_ITEM_REUSE_KEYS is defined at compile time the key
-          * will be stored and potentially reissued in the future.  If
-          * STORED_ITEM_REUSE_KEYS is NOT defined the key is retired.
-          *
-          * @param key Key which will be unregistered.
-          */
-
-         inline static void unregister(key_t& key)
-         {
-#ifdef STORED_ITEM_REUSE_KEYS
-            // make key generation thread safe
-
-            ThreadMutexGuard guard(getMutex());
-            keyPool().push(key);
-#endif           
-         }
-
-         /**
-          * @brief Return the unassigned key value.
-          *
-          * @return The unassigned key value.
-          */
-         
-         inline static const key_t& unassigned()
-         {
-            static const key_t sl_unassigned =
-               (keyLimits_t::has_denorm == std::denorm_present
-                ? keyLimits_t::denorm_min()
-                : keyLimits_t::min());
-            return sl_unassigned;
-         }
-
-         /**
-          * @brief Return the key increment value.
-          *
-          * @return The smallest allowable increment for a given key_t.
-          */
-
-         inline static const key_t& increment()
-         {
-            static const key_t sl_increment = 
-               (keyLimits_t::has_denorm == std::denorm_present
-                ? keyLimits_t::denorm_min()
-                : 1);
-            return sl_increment;
-         }
-
-         /**
-          * @brief Return the minimum key value.
-          *
-          * @return The smallest value of a key.
-          */
-
-         inline static const key_t& min()
-         {
-            static const key_t sl_min = unassigned() + increment();
-            return sl_min;
-         }
-
-         /**
-          * @brief Return the maximum key value.
-          *
-          * @return The largest value of a key.
-          */
-
-         inline static const key_t& max()
-         {
-            static const key_t sl_max = 
-               (keyLimits_t::has_denorm == std::denorm_present
-                ? keyLimits_t::min()
-                : keyLimits_t::max());
-            return sl_max;
-         }
-
-
-         /**
-          * @brief Return the key infinity value.
-          *
-          * @return The largest value of a key.
-          */
-
-         inline static const key_t& infinity()
-         {
-            static const key_t sl_infinity = 
-               (keyLimits_t::has_infinity
-                ? keyLimits_t::infinity()
-                : keyLimits_t::max());
-            return sl_infinity;
-         }
-
-      protected:
-
-         /**
-          * @brief Returns the value of the next key to be released.
-          */
-
-         inline static key_t & counter()
-         {
-            static key_t sl_nextKey = min();
-            return sl_nextKey;
-         }
-
-         /**
-          * @brief The mutex for thread safing.
-          */
-         
-         inline static ThreadMutex& getMutex()
-         {
-            static ThreadMutex sl_mutex;
-            return sl_mutex;
-         }
-
-#ifdef STORED_ITEM_REUSE_KEYS
-         /**
-          * @brief Stores unused keys for reuse.
-          */
-
-         inline static keyPool_t& keyPool()
-         {
-            static keyPool_t sl_keyPool;
-            return sl_keyPool;
-         }
-#endif         
-   };
 
    /** 
     * @class StoredItemKeyHasFunction
@@ -310,9 +85,6 @@ namespace PLEXIL
          }
    };
 
-//    template <class item_t>
-//    inline bool operator==(const item_t* i1, const item_t* i2);
-
    /**
     * @class StoredItem
     * @brief Provides for a symbolic value to be handled in a domain.
@@ -333,12 +105,6 @@ namespace PLEXIL
    class StoredItem
    {
       public:
-         
-//          template <class item_t>
-//          friend bool operator==(const item_t* i1, const item_t* i2)
-//          {
-//             return i1 == i2;
-//          }
    
          // define types for item and key store
          
