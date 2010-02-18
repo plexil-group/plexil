391ea9f94d85b3be0752bfc623cf3d17c1ea2ad1
Jonathan M. Hogins <jon.hogins@gmail.com>
2010-02-18 21:21:42 +0000

    Added library support to UniversalExec
    git-svn-id: https://svn.code.sf.net/p/plexil/code/trunk@1065 d2be67b0-814c-0410-949f-d215b6b29573

--- src/apps/UniversalExec/UniversalExec.cc
+++ src/apps/UniversalExec/UniversalExec.cc
@@ -1,28 +1,28 @@
 /* Copyright (c) 2006-2008, Universities Space Research Association (USRA).
-*  All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in the
-*       documentation and/or other materials provided with the distribution.
-*     * Neither the name of the Universities Space Research Association nor the
-*       names of its contributors may be used to endorse or promote products
-*       derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ *  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Universities Space Research Association nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <fstream>
 #include <time.h>
@@ -39,62 +39,59 @@
 #include "PlexilPlan.hh"
 #include "NewLuvListener.hh"
 
+using namespace PLEXIL;
 
 int main (int argc, char** argv)
 {
   std::string planName("error");
   std::string debugConfig("Debug.cfg");
   std::string interfaceConfig("interface-config.xml");
-  bool        luvRequest = false;
-  std::string luvHost    = PLEXIL::NewLuvListener::LUV_DEFAULT_HOSTNAME();
-  int         luvPort    = PLEXIL::NewLuvListener::LUV_DEFAULT_PORT();
-  bool        luvBlock   = false;
-  std::string usage(
-		    "Usage: universalExec -p <plan>\n\
+  std::vector<std::string> libraryNames;
+  bool luvRequest = false;
+  std::string luvHost = PLEXIL::NewLuvListener::LUV_DEFAULT_HOSTNAME();
+  int luvPort = PLEXIL::NewLuvListener::LUV_DEFAULT_PORT();
+  bool luvBlock = false;
+  std::string
+      usage(
+          "Usage: universalExec -p <plan>\n\
+                   [-l <library>]*\n\
                    [-c <interface_config_file>]\n\
                    [-d <debug_config_file>]\n\
                    [-v [-h <luv_hostname>] [-n <luv_portnumber>] -b]");
 
   // if not enough parameters, print usage
 
-  if (argc < 2)
-    {
-      std::cout << usage << std::endl;
+  if (argc < 2) {
+    std::cout << usage << std::endl;
+    return -1;
+  }
+  // parse out parameters
+
+  for (int i = 1; i < argc; ++i) {
+    if (strcmp(argv[i], "-b") == 0)
+      luvBlock = true;
+    else if (strcmp(argv[i], "-c") == 0)
+      interfaceConfig = std::string(argv[++i]);
+    else if (strcmp(argv[i], "-d") == 0)
+      debugConfig = std::string(argv[++i]);
+    else if (strcmp(argv[i], "-l") == 0)
+      libraryNames.push_back(argv[++i]);
+    else if (strcmp(argv[i], "-h") == 0)
+      luvHost = argv[++i];
+    else if (strcmp(argv[i], "-n") == 0) {
+      std::stringstream buffer;
+      buffer << argv[++i];
+      buffer >> luvPort;
+      SHOW(luvPort);
+    } else if (strcmp(argv[i], "-p") == 0)
+      planName = argv[++i];
+    else if (strcmp(argv[i], "-v") == 0)
+      luvRequest = true;
+    else {
+      std::cout << "Unknown option '" << argv[i] << "'.\n" << usage << std::endl;
       return -1;
     }
-  // parse out parameters
-   
-  for (int i = 1; i < argc; ++i)
-    {
-      if (strcmp(argv[i], "-b") == 0)
-	luvBlock = true;
-      else if (strcmp(argv[i], "-c") == 0)
-        interfaceConfig = std::string(argv[++i]);
-      else if (strcmp(argv[i], "-d") == 0)
-        debugConfig = std::string(argv[++i]);
-      else if (strcmp(argv[i], "-h") == 0)
-	luvHost = argv[++i];
-      else if (strcmp(argv[i], "-n") == 0)
-	{
-	  std::stringstream buffer;
-	  buffer << argv[++i];
-	  buffer >> luvPort;
-	  SHOW(luvPort);
-	}
-      else if (strcmp(argv[i], "-p") == 0)
-	planName = argv[++i];
-      else if (strcmp(argv[i], "-v") == 0)
-	luvRequest = true;
-      else
-	{
-	  std::cout << "Unknown option '" 
-		    << argv[i] 
-		    << "'.\n" 
-		    << usage 
-		    << std::endl;
-	  return -1;
-	}
-    }
+  }
   // basic initialization
 
   std::ifstream dbgConfig(debugConfig.c_str());
@@ -103,24 +100,19 @@ int main (int argc, char** argv)
 
   // get interface configuration file, if provided
   TiXmlDocument* configDoc = NULL;
-  if (!interfaceConfig.empty())
-    {
-      std::cout << "Reading interface configuration from "
-                << interfaceConfig
-                << std::endl;
-      configDoc = new TiXmlDocument(interfaceConfig);
-      if (!configDoc->LoadFile())
-        {
-          std::cout << "WARNING: unable to load interface configuration file "
-                    << interfaceConfig
-                    << ":\n "
-                    << configDoc->ErrorDesc()
-                    <<"\nContinuing without interface configuration"
-                    << std::endl;
-          delete configDoc;
-          configDoc = NULL;
-        }
+  if (!interfaceConfig.empty()) {
+    std::cout << "Reading interface configuration from " << interfaceConfig << std::endl;
+    configDoc = new TiXmlDocument(interfaceConfig);
+    if (!configDoc->LoadFile()) {
+      std::cout << "WARNING: unable to load interface configuration file " 
+      << interfaceConfig 
+      << ":\n " 
+      << configDoc->ErrorDesc()
+         << "\nContinuing without interface configuration" << std::endl;
+      delete configDoc;
+      configDoc = NULL;
     }
+  }
 
   // get Interfaces element
   TiXmlElement* configElt = NULL;
@@ -171,23 +163,30 @@ int main (int argc, char** argv)
   std::cout << "Starting the exec" << std::endl;
   _app.run();
 
-  if (planName != "error")
-    {
-      TiXmlDocument plan(planName);
-      if (!plan.LoadFile())
-	{
-	  std::cout << "Error parsing plan '"
-		    << planName << "': " 
-		    << plan.ErrorDesc()
-		    << " line "
-		    << plan.ErrorRow() 
-		    << " column " 
-		    << plan.ErrorCol()
-		    << std::endl;
-	  return -1;
-	}
-      _app.addPlan(&plan);
+  // if specified on command line, load libraries
+
+  for (std::vector<std::string>::const_iterator libraryName = libraryNames.begin(); libraryName != libraryNames.end(); ++libraryName) {
+    TiXmlDocument libraryXml(*libraryName);
+    if (!libraryXml.LoadFile()) {
+      std::cout << "XML error parsing library '" << *libraryName << "': " << libraryXml.ErrorDesc() << " line " << libraryXml.ErrorRow() << " column "
+          << libraryXml.ErrorCol() << std::endl;
+      return -1;
     }
+
+    if (!_app.addLibrary(&libraryXml)) {
+      std::cout << "ERROR: unable to add library " << *libraryName << std::endl;
+      return -1;
+    }
+  }
+
+  if (planName != "error") {
+    TiXmlDocument plan(planName);
+    if (!plan.LoadFile()) {
+      std::cout << "Error parsing plan '" << planName << "': " << plan.ErrorDesc() << " line " << plan.ErrorRow() << " column " << plan.ErrorCol() << std::endl;
+      return -1;
+    }
+    _app.addPlan(&plan);
+  }
 
   _app.waitForPlanFinished();
 
@@ -195,7 +194,4 @@ int main (int argc, char** argv)
 
   return 0;
 }
-
-
-
 
