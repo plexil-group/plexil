91e2092778bc7a10ff3c292f0958fce999b756ff
Jason G. Ho <jasongho85@gmail.com>
2010-06-08 19:41:42 +0000

    LuvTempFile class
    git-svn-id: https://svn.code.sf.net/p/plexil/code/trunk@1367 d2be67b0-814c-0410-949f-d215b6b29573

---
+++ src/luv/src/gov/nasa/luv/LuvTempFile.java
@@ -0,0 +1,42 @@
+package gov.nasa.luv;
+import java.io.IOException;
+import java.io.File;
+import static gov.nasa.luv.Constants.PORT_TEMP_FILE_LOCATION;
+
+public class LuvTempFile {
+	
+	LuvTempFile() {
+		File temp_dir = new File(PORT_TEMP_FILE_LOCATION);
+		File temp = new File(PORT_TEMP_FILE_LOCATION + "Luv_" + Luv.getLuv().getPort());		
+		try{
+		  if(!temp_dir.exists())
+			  throw new IllegalArgumentException("Undefined temporary file directory" + temp_dir.getName()); 
+		  if(temp.exists())
+			  throw new IllegalArgumentException("Port in use");
+		  else{
+			  temp.createNewFile();
+			  temp.deleteOnExit();
+		  }
+		  
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+public static void deleteTempFile() {
+	
+	String fileName = PORT_TEMP_FILE_LOCATION + "Luv_" + Luv.getLuv().getPort();
+	File f = new File(fileName);
+	
+	if (!f.exists())
+		return;
+	
+	if(!f.canWrite())
+		throw new IllegalArgumentException("Delete: write protected: " + fileName);
+	
+	boolean success = f.delete();
+	
+	if(!success)
+		throw new IllegalArgumentException("Delete: deletion failed");
+	}
+}
