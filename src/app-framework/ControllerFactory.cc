b76fd9529d5f8fc6a1c5b13a750a0eb3dfae1cfd
Chuck Fry <chuck.fry@nasa.gov>
2010-07-21 23:35:11 +0000

    New; abstract factory for ExecController instances
    git-svn-id: https://svn.code.sf.net/p/plexil/code/trunk@1434 d2be67b0-814c-0410-949f-d215b6b29573

---
+++ src/app-framework/ControllerFactory.cc
@@ -0,0 +1,173 @@
+/* Copyright (c) 2006-2010, Universities Space Research Association (USRA).
+*  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Universities Space Research Association nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "ControllerFactory.hh"
+#include "ExecController.hh"
+#include "ExecApplication.hh"
+#include "DynamicLoader.hh"
+#include "InterfaceSchema.hh"
+
+#ifndef TIXML_USE_STL
+#define TIXML_USE_STL
+#endif
+#include "tinyxml.h"
+
+namespace PLEXIL
+{
+  /**
+   * @brief Creates a new ExecController instance as specified by
+   *        the given configuration XML.
+   * @param name The registered name for the factory.
+   * @param xml The configuration XML to be passed to the ExecController constructor.
+   * @param execInterface Reference to the parent ExecApplication instance.
+   * @return The Id for the new ExecController.  May not be unique.
+   */
+
+  ExecControllerId 
+  ControllerFactory::createInstance(const TiXmlElement* xml,
+                                    ExecApplication& execInterface)
+  {
+    // Can't do anything without the spec
+    assertTrueMsg(xml != NULL,
+                  "ControllerFactory::createInstance: null configuration XML");
+
+    // Get the kind of controller to make
+    const char* controllerType = 
+      xml->Attribute(InterfaceSchema::CONTROLLER_TYPE_ATTR());
+    checkError(controllerType != 0,
+               "ControllerFactory::createInstance: no "
+               << InterfaceSchema::CONTROLLER_TYPE_ATTR()
+               << " attribute for controller XML:\n"
+               << *xml);
+
+    // Make it
+    bool dummy;
+    return createInstance(LabelStr(controllerType), xml, execInterface, dummy);
+  }
+
+  /**
+   * @brief Creates a new ExecController instance with the type associated with the name and
+   *        the given configuration XML.
+   * @param name The registered name for the factory.
+   * @param xml The configuration XML to be passed to the ExecController constructor.
+   * @param execInterface Reference to the parent ExecApplication instance.
+   * @return The Id for the new ExecController.  May not be unique.
+   */
+
+  ExecControllerId 
+  ControllerFactory::createInstance(const LabelStr& name,
+                                    const TiXmlElement* xml,
+                                    ExecApplication& execInterface)
+  {
+    bool dummy;
+    return createInstance(name, xml, execInterface, dummy);
+  }
+
+
+  /**
+   * @brief Creates a new ExecController instance with the type associated with the name and
+   *        the given configuration XML.
+   * @param name The registered name for the factory.
+   * @param xml The configuration XML to be passed to the ExecController constructor.
+   * @param wasCreated Reference to a boolean variable;
+   *                   variable will be set to true if new object created, false otherwise.
+   * @return The Id for the new ExecController.  If wasCreated is set to false, is not unique.
+   */
+
+  ExecControllerId 
+  ControllerFactory::createInstance(const LabelStr& name,
+                                    const TiXmlElement* xml,
+                                    ExecApplication& execInterface,
+                                    bool& wasCreated)
+  {
+    std::map<double, ControllerFactory*>::const_iterator it = factoryMap().find(name.getKey());
+    if (it == factoryMap().end())
+      {
+        debugMsg("ControllerFactory:createInstance", 
+                 "Attempting to dynamically load controller type \""
+                 << name.c_str() << "\"");
+        // Attempt to dynamically load library
+        const char* libCPath =
+          xml->Attribute(InterfaceSchema::LIB_PATH_ATTR());
+        assertTrueMsg(DynamicLoader::loadModule(name.c_str(), libCPath),
+                      "ControllerFactory::createInstance: unable to load module for controller type \""
+                      << name.c_str() << "\"");
+
+        // See if it's registered now
+        it = factoryMap().find(name.getKey());
+      }
+
+    assertTrueMsg(it != factoryMap().end(),
+                  "Error: No controller factory registered for name \"" << name.c_str() << "\".");
+    ExecControllerId retval = it->second->create(xml, execInterface, wasCreated);
+    debugMsg("ControllerFactory:createInstance", " Created controller " << name.c_str());
+    return retval;
+  }
+
+  bool ControllerFactory::isRegistered(const LabelStr& name) {
+    return factoryMap().find(name) != factoryMap().end();
+  }
+
+  std::map<double, ControllerFactory*>& ControllerFactory::factoryMap() 
+  {
+    static std::map<double, ControllerFactory*> sl_map;
+    return sl_map;
+  }
+
+  /**
+   * @brief Deallocate all factories
+   */
+  void ControllerFactory::purge()
+  {
+    for (std::map<double, ControllerFactory*>::iterator it = factoryMap().begin();
+         it != factoryMap().end();
+         ++it)
+      delete it->second;
+    factoryMap().clear();
+  }
+
+  /**
+   * @brief Registers an ControllerFactory with the specific name.
+   * @param name The name by which the Controller shall be known.
+   * @param factory The ControllerFactory instance.
+   */
+  void ControllerFactory::registerFactory(const LabelStr& name, ControllerFactory* factory)
+  {
+    assertTrue(factory != NULL);
+    if (factoryMap().find(name.getKey()) != factoryMap().end())
+      {
+        warn("Attempted to register an controller factory for name \""
+             << name.c_str()
+             << "\" twice, ignoring.");
+        delete factory;
+        return;
+      }
+    factoryMap()[name.getKey()] = factory;
+    debugMsg("ControllerFactory:registerFactory",
+             " Registered controller factory for name \"" << name.c_str() << "\"");
+  }
+
+}
