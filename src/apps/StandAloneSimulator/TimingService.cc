7422b86778f1fda56794bc1b71f1e2ffe45a7354
Chuck Fry <chuck.fry@nasa.gov>
2010-09-16 21:56:33 +0000

    Eliminate callbacks, add new wait() method; other cleanups including signal handling improvements
    git-svn-id: https://svn.code.sf.net/p/plexil/code/trunk@1531 d2be67b0-814c-0410-949f-d215b6b29573

--- src/apps/StandAloneSimulator/TimingService.cc
+++ src/apps/StandAloneSimulator/TimingService.cc
@@ -34,90 +34,228 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <iostream>
+#include <iomanip>
 
-TimingService::TimerCallbackFn TimingService::s_Callback = NULL;
-void* TimingService::s_CallbackArg = NULL;
-
-TimingService::TimingService(TimerCallbackFn _callback, void* _callbackArg) 
-  : m_TimerSetup(false)
+TimingService::TimingService() 
+  : m_nBlockedSignals(0)
 {
-  s_Callback = _callback;
-  s_CallbackArg = _callbackArg;
-  setupTimer();
+  // clear out signal handler storage
+  // (not strictly necessary, but better safe than sorry, and it's cheap)
+  for (size_t i = 0; i <= TIMING_SERVICE_MAX_N_SIGNALS; i++) {
+	m_blockedSignals[i] = 0;
+  }
 }
 
 TimingService::~TimingService()
 {
   stopTimer();
+  if (m_nBlockedSignals != 0)
+	restoreSignalHandling();
 }
 
-void TimingService::setupTimer()
+/**
+ * @brief Dummy signal handler function for signals we process.
+ * @note This should never be called!
+ */
+void dummySignalHandler(int /* signo */) {}
+
+/**
+ * @brief Set the process's signal mask and signal actions for timer use.
+ * @param signalsToIgnore Zero-terminated array of signals to be handled by wait().
+ * @return The status of the sigprocmask call; 0 = success.
+ * @note TimingService clients MUST call this or defaultInitializeSignalHandling() prior to arming the timer!
+ * @note SIGALRM will never be returned from a wait() call, as TimingService uses it internally.
+ */
+int TimingService::initializeSignalHandling(const int signumsToIgnore[])
 {
+  //
+  // Generate the mask
+  // N.B. the same mask is used in the sigwait() call.
+  //
+  int errnum = sigemptyset(&m_sigset);
+  if (errnum != 0) {
+	debugMsg("TimingService:initializeSignalHandling", " sigemptyset returned " << errnum);
+	return errnum;
+  }
+  for (m_nBlockedSignals = 0;
+	   m_nBlockedSignals < TIMING_SERVICE_MAX_N_SIGNALS && signumsToIgnore[m_nBlockedSignals] != 0;
+	   m_nBlockedSignals++) {
+	int sig = signumsToIgnore[m_nBlockedSignals];
+	m_blockedSignals[m_nBlockedSignals] = sig; // save to restore it later
+	errnum = sigaddset(&m_sigset, sig);
+	if (errnum != 0) {
+	  debugMsg("TimingService:initializeSignalHandling", " sigaddset returned " << errnum);
+	  return errnum;
+	}
+  }
+  // add SIGALRM to mask
+  m_blockedSignals[m_nBlockedSignals++] = SIGALRM;
+  errnum = sigaddset(&m_sigset, SIGALRM);
+  if (errnum != 0) {
+	debugMsg("TimingService:initializeSignalHandling", " sigaddset returned " << errnum);
+	return errnum;
+  }
+
+  // Set the mask
+  errnum = sigprocmask(SIG_BLOCK, &m_sigset, &m_restoreSigset);
+  if (errnum != 0) {
+	debugMsg("TimingService:initializeSignalHandling", " sigprocmask returned " << errnum);
+	return errnum;
+  }
+
+  //
+  // Install signal handlers
+  //
   struct sigaction sa;
-  
-  memset (&sa, 0, sizeof (sa));
-  sa.sa_handler = &TimingService::timerHandler;
-  sigaction(SIGALRM, &sa, &m_oldSigaction);
-  
-  m_Timer.it_interval.tv_sec = m_Timer.it_interval.tv_usec = 0;
-  m_TimerSetup = true;
+  sigemptyset(&sa.sa_mask); // *** is this enough?? ***
+  sa.sa_flags = 0;
+  sa.sa_handler = dummySignalHandler;
+
+  for (size_t i = 0; i < m_nBlockedSignals; i++) {
+	errnum = sigaction(m_blockedSignals[i], &sa, &(m_restoreHandlers[i]));
+	if (errnum != 0) {
+	  debugMsg("TimingService:initializeSignalHandling", " sigaction returned " << errnum);
+	  return errnum;
+	}
+  }
+	   
+  debugMsg("TimingService:initializeSignalHandling", " complete");
+  return 0;
 }
 
-void TimingService::timerHandler (int signum)
+/**
+ * @brief Set the process's signal mask to a convenient default for timer use.
+ * @return True if successful, false otherwise.
+ * @note TimingService clients MUST call this or initializeSignalHandling() prior to arming the timer!
+ * @note Calls initializeSignalHandling() internally.
+ */
+bool TimingService::defaultInitializeSignalHandling()
 {
-  checkError(signum == SIGALRM,
-	     "TimingService: Fatal error: Handler function called with wrong signal # " << signum);
-  assertTrueMsg(s_Callback != NULL,
-		"TimingService: Fatal error: Callback is null at wakeup!");
-  (s_Callback)(s_CallbackArg);
+  static int defaultSignals[TIMING_SERVICE_MAX_N_SIGNALS] =
+	{SIGINT, SIGHUP, SIGQUIT, SIGTERM, 0, 0, 0, 0};
+  int errnum = initializeSignalHandling(defaultSignals);
+  condDebugMsg(errnum != 0,
+			   "TimingService:defaultInitializeSignalHandling",
+			   " initializeSignalHandling failed, result = " << errnum);
+  return errnum == 0;
+}
+
+/**
+ * @brief Restore the process's original signal mask to its state at the last initializeSignalHandling() call.
+ * @return True if successful, false otherwise.
+ */
+bool TimingService::restoreSignalHandling()
+{
+  //
+  // Restore old mask
+  //
+  int errnum = sigprocmask(SIG_SETMASK, &m_restoreSigset, NULL);
+  assertTrueMsg(errnum == 0, 
+				"TimingService::restoreSignalHandling: Fatal error: sigprocmask returned " << errnum);
+
+  //
+  // Restore old signal handlers
+  //
+  for (size_t i = 0; i < m_nBlockedSignals; i++) {
+	errnum = sigaction(m_blockedSignals[i], &m_restoreHandlers[i], NULL);
+	if (errnum != 0) {
+	  debugMsg("TimingService:restoreSignalHandling", " sigaction returned " << errnum);
+	  return errnum;
+	}
+  }
+
+  // flag as complete
+  m_nBlockedSignals = 0;
+
+  debugMsg("TimingService:restoreSignalHandling", " complete");
+  return 0;
 }
 
 bool TimingService::setTimer(const timeval& time)
 {
-  if (m_TimerSetup)
-    {
-      timeval cTime;
-      gettimeofday(&cTime, NULL);
-      m_Timer.it_value = time - cTime;
-      
-      if (m_Timer.it_value.tv_sec < 0 || m_Timer.it_value.tv_usec < 0)
-        return true;
-      
-      int status = setitimer(ITIMER_REAL, &m_Timer, NULL);
-      if (status == -1)
-        {
-          std::cerr << "TimingService::setTimer Error " << errno << " while setting timer." << std::endl;
-          perror("Error for setitimer");
-        }
-      else
-        {
-          debugMsg("TimingService:setTimer",
-		   " Set interval timer for: " << m_Timer.it_value.tv_sec << "(sec) "
-		   << m_Timer.it_value.tv_usec << "(usec).");
-        }
-    }
-  else
-    std::cerr << "Error: TimingService::setTimer. Timer has not been set up." << std::endl;
+  assertTrueMsg(m_nBlockedSignals != 0,
+				"TimingService::setTimer: Fatal error: signal handling has not been initialized");
 
-  return false;
+  timeval now;
+  gettimeofday(&now, NULL);
+  itimerval myTimer;
+  myTimer.it_interval.tv_sec = myTimer.it_interval.tv_usec = 0;
+  myTimer.it_value = time - now;
+      
+  if (myTimer.it_value.tv_sec < 0 || myTimer.it_value.tv_usec < 0) {
+	debugMsg("TimingService:setTimer",
+			 " Not setting interval timer, requested time is in the past");
+	return false;
+  }
+      
+  int status = setitimer(ITIMER_REAL, &myTimer, NULL);
+  assertTrueMsg(status == 0,
+				"TimingService::setTimer: Fatal error: setitimer failed, errno = " << errno);
+  debugMsg("TimingService:setTimer",
+		   " Set interval timer for "
+		   << std::setiosflags(std::ios_base::fixed) 
+		   << timevalToDouble(myTimer.it_value));
+  return true;
+}
+
+/**
+ * @brief Get the (approximate) absolute time of the next scheduled wakeup.
+ * @param result A reference to the variable where the interval should be stored.
+ */
+void TimingService::getTimer(timeval& result) 
+{
+  itimerval itime;
+  int status = getitimer(ITIMER_REAL, &itime);
+  assertTrueMsg(status == 0, 
+				"TimingService::getTimer: Fatal error: getitimer failed, status = " << status);
+  if (itime.it_value.tv_sec == 0 && itime.it_value.tv_usec == 0) {
+	debugMsg("TimingService:getTimer", " timer is not set");
+	result.tv_sec = result.tv_usec = 0;
+	return;
+  }
+
+  // compute result based on current time of day
+  timeval now;
+  status = gettimeofday(&now, NULL);
+  assertTrueMsg(status == 0, 
+				"TimingService::getTimer: Fatal error: gettimeofday failed, status = " << status);
+  result = now + itime.it_value;
+  debugMsg("TimingService:getTimer",
+		   " timer set for "
+		   << std::setiosflags(std::ios_base::fixed) 
+		   << timevalToDouble(result));
 }
 
 void TimingService::stopTimer()
 {
-  if (m_TimerSetup)
-    {
-      debugMsg("TimingService:stopTimer", " disabling timer interrupts");
-      m_Timer.it_interval.tv_sec = m_Timer.it_interval.tv_usec = 0;
-      int status = setitimer(ITIMER_REAL, &m_Timer, NULL);
-      if (status == -1)
-        {
-          std::cerr << "TimingService:stopTimer Error " << errno << " while stopping timer." << std::endl;
-          perror("Error for setitimer");
-        }
+  debugMsg("TimingService:stopTimer", " disabling timer");
+  itimerval myTimer;
+  myTimer.it_interval.tv_sec = myTimer.it_interval.tv_usec = 0;
+  myTimer.it_value.tv_sec = myTimer.it_value.tv_usec = 0;
+  int status = setitimer(ITIMER_REAL, &myTimer, NULL);
+  assertTrueMsg(status == 0,
+				"TimingService::stopTimer: Fatal error: setitimer failed, errno = " << errno);
+}
 
-      debugMsg("TimingService:stopTimer", " restoring old SIGALRM handler");
-      sigaction(SIGALRM, &m_oldSigaction, NULL);
-      m_TimerSetup = false;
-    }
+int TimingService::wait()
+{
+  assertTrueMsg(m_nBlockedSignals != 0,
+				"TimingService::wait: Fatal error: signal handling has not been initialized");
+
+  debugMsg("TimingService:wait", " entered");
+
+  int theSignal;
+  int errnum = sigwait(&m_sigset, &theSignal);
+  // Check status
+  assertTrueMsg(errnum == 0, 
+				"TimingService::wait: Fatal error: sigwait returned non-zero status " << errnum);
+
+  // Check the signal number
+  if (theSignal == SIGALRM) {
+	debugMsg("TimingService:wait", " received timer wakeup");
+	return 0;
+  }
+
+  debugMsg("TimingService:wait", " received non-timer signal " << theSignal);
+  return theSignal;
 }
--- src/apps/StandAloneSimulator/TimingService.hh
+++ src/apps/StandAloneSimulator/TimingService.hh
@@ -30,30 +30,80 @@
 #include <sys/time.h>
 #include <csignal>
 
+#define TIMING_SERVICE_MAX_N_SIGNALS 8
+
 class TimingService
 {
 public:
-  typedef void(*TimerCallbackFn)(void*) ;
-  TimingService(TimerCallbackFn _callBack, void* _callbackArg);
+  TimingService();
   ~TimingService();
-  static void timerHandler (int signum);
+
+  /**
+   * @brief Set the process's signal mask and signal actions for timer use.
+   * @param signalsToIgnore Zero-terminated array of signal numbers to be handled by wait().
+   * @return The status of the sigprocmask call; 0 = success.
+   * @note TimingService clients MUST call this or defaultInitializeSignalHandling() prior to arming the timer!
+   * @note SIGALRM will never be returned from a wait() call, as TimingService uses it internally.
+   * @note No more than TIMING_SERVICE_MAX_N_SIGNALS will be processed.
+   */
+  int initializeSignalHandling(const int signumsToIgnore[]);
+
+  /**
+   * @brief Set the process's signal mask to a convenient default for timer use.
+   * @return True if successful, false otherwise.
+   * @note TimingService clients MUST call this or initializeSignalHandling() prior to arming the timer!
+   * @note Calls initializeSignalHandling() internally.
+   */
+  bool defaultInitializeSignalHandling();
+
+  /**
+   * @brief Restore the process's original signal mask to its state at the last initializeSignalHandling() call.
+   * @return True if successful, false otherwise.
+   */
+  bool restoreSignalHandling();
+
+  /**
+   * @brief Set the timer for the specified absolute time.
+   * @param time Const reference to a timeval with the requested wakeup time.
+   * @return True if the time is in the future, false otherwise.
+   */
   bool setTimer(const timeval& time);
+
+  /**
+   * @brief Get the (estimated) absolute time of the next scheduled wakeup.
+   * @param result A reference to the variable where the interval should be stored.
+   */
+  void getTimer(timeval& result);
+
+  /**
+   * @brief Disable the timer.
+   */
   void stopTimer();
 
+  /**
+   * @brief Wait for a timer wakeup.
+   * @return 0 if the wakeup was due to the timer, the signal number otherwise.
+   * @note Signals like SIGINT, SIGHUP are caught by this method;
+   *       dealing with them properly is the caller's responsibility.
+   */
+  int wait();
+
 private:  
+
   // Deliberately not implemented
-  TimingService();
   TimingService(const TimingService&);
   TimingService& operator=(const TimingService&);
 
-  void setupTimer();
+  //
+  // Member variables
+  //
 
-  static TimerCallbackFn s_Callback;
-  static void* s_CallbackArg;
-
-  struct itimerval m_Timer;
-  struct sigaction m_oldSigaction;
-  bool m_TimerSetup;
+  // Signal handling
+  size_t m_nBlockedSignals;
+  int m_blockedSignals[TIMING_SERVICE_MAX_N_SIGNALS + 1];
+  struct sigaction m_restoreHandlers[TIMING_SERVICE_MAX_N_SIGNALS + 1];
+  sigset_t m_sigset;
+  sigset_t m_restoreSigset;
 };
 
 #endif
