d08836088ee60ab3323df816e9a6180166cfb2f4
Chuck Fry <chuck.fry@nasa.gov>
2011-05-13 21:38:58 +0000

    New, formerly private inner class of InterfaceManager; implemented as linked list
    git-svn-id: https://svn.code.sf.net/p/plexil/code/trunk@2136 d2be67b0-814c-0410-949f-d215b6b29573

---
+++ src/app-framework/ValueQueue.cc
@@ -0,0 +1,236 @@
+/* Copyright (c) 2006-2011, Universities Space Research Association (USRA).
+*  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Universities Space Research Association nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "ValueQueue.hh"
+
+namespace PLEXIL
+{
+
+  //
+  // ValueQueue implementation
+  //
+  
+  ValueQueue::ValueQueue()
+    : m_head(),
+	  m_tail(),
+	  m_freeList(),
+      m_mutex(new ThreadMutex()),
+	  m_markCount(0)
+  {
+  }
+
+  ValueQueue::~ValueQueue()
+  {
+	// Free the queue entries, if any
+	while (m_head.isId())
+	  delete (QueueEntry*) behead();
+
+	// Free the free-list entries, if any
+	while (m_freeList.isId()) {
+	  QueueEntryId entry = m_freeList;
+	  m_freeList = entry->next;
+	  delete (QueueEntry*) entry;
+	}
+
+	// Free the mutex
+    delete m_mutex;
+  }
+
+  void ValueQueue::enqueue(const ExpressionId & exp,
+						   double newValue)
+  {
+    ThreadMutexGuard guard(*m_mutex);
+	QueueEntryId e = allocate();
+	e->type = queueEntry_RETURN_VALUE;
+	e->expression = exp;
+	e->values.push_back(newValue);
+	insert(e);
+  }
+
+  void ValueQueue::enqueue(const StateKey& key, 
+						   const std::vector<double> & newValues)
+  {
+    ThreadMutexGuard guard(*m_mutex);
+	QueueEntryId e = allocate();
+	e->type = queueEntry_LOOKUP_VALUES;
+	e->stateKey = key;
+	e->values = newValues;
+	insert(e);
+  }
+
+  void ValueQueue::enqueue(PlexilNodeId newPlan,
+						   const LabelStr & parent)
+  {
+    ThreadMutexGuard guard(*m_mutex);
+	QueueEntryId e = allocate();
+	e->type = queueEntry_PLAN;
+	e->plan = newPlan;
+	e->parent = parent;
+	insert(e);
+  }
+
+  void ValueQueue::enqueue(PlexilNodeId newLibraryNode)
+  {
+    ThreadMutexGuard guard(*m_mutex);
+	QueueEntryId e = allocate();
+	e->type = queueEntry_LIBRARY;
+	e->plan = newLibraryNode;
+	insert(e);
+  }
+
+  QueueEntryType
+  ValueQueue::dequeue(StateKey& stateKey, std::vector<double>& newStateValues,
+					  ExpressionId& exp, double& newExpValue,
+					  PlexilNodeId& plan, LabelStr& planParent,
+					  unsigned int& sequence)
+  {
+    ThreadMutexGuard guard(*m_mutex);
+    QueueEntryId e = behead();
+    if (e.isNoId())
+      return queueEntry_EMPTY;
+
+	QueueEntryType eType = e->type;
+    switch (eType) {
+	case queueEntry_MARK:
+	  sequence = e->sequence;
+	  break;
+
+	case queueEntry_LOOKUP_VALUES:
+	  stateKey = e->stateKey;
+	  newStateValues = e->values;
+	  break;
+
+	case queueEntry_RETURN_VALUE:
+	  checkError(e->values.size() == 1,
+				 "ValueQueue::dequeue: Invalid number of values for return value entry");
+	  exp = e->expression;
+	  newExpValue = e->values[0];
+	  break;
+
+	case queueEntry_PLAN:
+	  planParent = e->parent;
+	  // fall thru to library case
+
+	case queueEntry_LIBRARY:
+	  plan = e->plan;
+	  break;
+
+	default:
+	  assertTrue(ALWAYS_FAIL,
+				 "ValueQueue::dequeue: Invalid queue entry");
+	  break;
+	}
+
+	// We're done with the head, return it to free list
+	free(e);
+
+	// Return the entry type
+    return eType;
+  }
+
+  void ValueQueue::pop()
+  {
+    ThreadMutexGuard guard(*m_mutex);
+	QueueEntryId head = behead();
+	if (head.isId())
+	  free(head);
+  }
+
+  bool ValueQueue::isEmpty() const
+  {
+    ThreadMutexGuard guard(*m_mutex);
+    return m_head.isNoId();
+  }
+    
+  unsigned int ValueQueue::mark()
+  {
+    ThreadMutexGuard guard(*m_mutex);
+	QueueEntryId theMark = allocate();
+	theMark->type = queueEntry_MARK;
+	theMark->sequence = ++m_markCount;
+	insert(theMark);
+	return m_markCount;
+  }
+
+  //
+  // Queue internal methods
+  // All assume calling thread is holding the mutex
+  //
+
+  // Insertion
+  void ValueQueue::insert(QueueEntryId entry)
+  {
+	if (m_head.isId()) {
+	  // Queue not empty
+	  m_tail->next = entry;
+	  m_tail = entry;
+	}
+	else {
+	  // Queue is empty
+	  m_head = m_tail = entry;
+	}
+  }
+
+  // Removal
+  ValueQueue::QueueEntryId ValueQueue::behead()
+  {
+	QueueEntryId result = m_head;
+	if (result.isNoId())
+	  // already empty, nothing more to do
+	  return result;
+	m_head = result->next;
+	if (m_head.isNoId()) {
+	  // Queue is now empty, clear tail
+	  m_tail = m_head;
+	}
+	else {
+	  result->next = QueueEntryId::noId();
+	}
+	return result;
+  }
+
+  // Free list management
+
+  ValueQueue::QueueEntryId ValueQueue::allocate()
+  {
+	if (m_freeList.isNoId()) 
+	  return (new QueueEntry())->getId();
+
+	// Manage free list in last-in, first-out fashion
+	QueueEntryId result = m_freeList;
+	m_freeList = result->next;
+	result->next = QueueEntryId::noId();
+	return result;
+  }
+  
+  void ValueQueue::free(QueueEntryId entry)
+  {
+	entry->clear();
+	entry->next = m_freeList;
+	m_freeList = entry;
+  }
+
+}
---
+++ src/app-framework/ValueQueue.hh
@@ -0,0 +1,222 @@
+/* Copyright (c) 2006-2011, Universities Space Research Association (USRA).
+*  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Universities Space Research Association nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef VALUE_QUEUE_HH
+#define VALUE_QUEUE_HH
+
+#include "Expression.hh"
+
+namespace PLEXIL
+{
+
+  //
+  // Value queue
+  //
+
+  /**
+   * @brief Represents the type of a ValueQueue::QueueEntry instance.
+   */
+  enum QueueEntryType {
+	queueEntry_EMPTY,
+	queueEntry_MARK,
+	queueEntry_LOOKUP_VALUES,
+	queueEntry_RETURN_VALUE,
+	queueEntry_PLAN,
+	queueEntry_LIBRARY,
+	queueEntry_ERROR
+  };
+
+  /**
+   * @brief A private internal class where the InterfaceManager temporarily
+   *        stores the results of asynchronous operations on the world outside
+   *        the Exec.
+   */
+  class ValueQueue
+  {
+  public:
+	ValueQueue();
+	~ValueQueue();
+
+	// Inserts the new expression/value pair into the queue
+	void enqueue(const ExpressionId & exp, double newValue);
+	void enqueue(const StateKey & stateKey, const std::vector<double> & newValues);
+	void enqueue(PlexilNodeId newPlan, const LabelStr & parent);
+	void enqueue(PlexilNodeId newlibraryNode);
+
+	/**
+	 * @brief Atomically check head of queue and dequeue if appropriate
+	 * @return Type of entry dequeued; queueEntry_EMPTY and queueEntry_MARK
+	 * indicate nothing of interest was dequeued
+	 */
+	QueueEntryType dequeue(StateKey& stateKey, std::vector<double>& newStateValues,
+						   ExpressionId& exp, double& newExpValue,
+						   PlexilNodeId& plan, LabelStr& planParent,
+						   unsigned int& sequence);
+
+	// returns true iff the queue is empty
+	bool isEmpty() const;
+
+	//* Inserts a marker expression into the queue and returns its sequence number
+	unsigned int mark();
+
+	// Remove queue head and ignore (presumably a mark)
+	void pop();
+
+  private:
+	// deliberately unimplemented
+	ValueQueue(const ValueQueue &);
+	ValueQueue & operator=(const ValueQueue &);
+
+	//
+	// Internal methods
+	//
+	class QueueEntry;
+	typedef Id<QueueEntry> QueueEntryId;
+
+	//* Insert the given entry into the queue.
+	void insert(QueueEntryId entry);
+
+	//* Remove the current head and return it.
+	QueueEntryId behead();
+
+	//* Add the entry to the free list.
+	void free(QueueEntryId entry);
+
+	//* Allocate from the free list if possible, else construct a new entry.
+	QueueEntryId allocate();
+
+	//
+	// Member variables
+	//
+
+	DECLARE_STATIC_CLASS_CONST(State, 
+							   NULL_STATE_KEY,
+							   State(0.0, std::vector<double>()));
+
+	/**
+	 * @brief Represents one entry in a ValueQueue.  
+	 *        A private class internal to ValueQueue.
+	 */
+	class QueueEntry
+	{
+	public:
+	  /*
+	   * @brief Constructor for an empty QueueEntry of an arbitrary QueueEntryType.
+	   */
+	  QueueEntry()
+		: id(this),
+		  next(),
+		  type(queueEntry_EMPTY),
+		  expression(),
+		  stateKey(),
+		  values(),
+		  plan(),
+		  parent(EMPTY_LABEL()),
+		  sequence(0)
+	  {
+	  }
+
+	  /**
+	   * @brief Destructor.
+	   */
+	  ~QueueEntry()
+	  {
+	  }
+
+	  /**
+	   * @brief Reset to initialized state for reuse.
+	   */
+	  void clear()
+	  {
+		// Only clear the fields that were active
+		switch (type) {
+		case queueEntry_RETURN_VALUE:
+		  expression = ExpressionId::noId();
+		  values.clear();
+		  break;
+			
+		case queueEntry_LOOKUP_VALUES:
+		  stateKey = 0.0;
+		  values.clear();
+		  break;
+
+		case queueEntry_PLAN:
+		  parent = EMPTY_LABEL();
+		  // fall through to...
+		case queueEntry_LIBRARY:
+		  plan = PlexilNodeId::noId();
+		  break;
+
+		case queueEntry_MARK:
+		  sequence = 0;
+		  break;
+
+		default:
+		  break;
+		}
+		// Now can clear type
+		type = queueEntry_EMPTY;
+	  }
+
+	  const QueueEntryId& getId() const
+	  {
+		return id;
+	  }
+	
+	  QueueEntryId id; //* ID of this entry.
+	  QueueEntryId next; //* Link to the next queue (or free list) entry.
+	  QueueEntryType type; //* What kind of queue entry this is.
+	  // Data for value queue
+	  ExpressionId expression; //* The expression to which this value belongs for command returns.
+	  StateKey stateKey; //* The state to which this value belongs for lookup returns.
+	  std::vector<double> values; //* The values being returned.
+	  // Data for plan queue
+	  PlexilNodeId plan; //* The intermediate representation of this plan or library node.
+	  LabelStr parent; //* The parent node ID under which to store the plan (NYI).
+	  // Data for mark
+	  unsigned int sequence; //* The count of marks issued to date.
+	};
+
+	//
+	// Queue data
+	//
+	QueueEntryId m_head; //* The next entry to be dequeued.
+	QueueEntryId m_tail; //* The current end of the queue.
+	QueueEntryId m_freeList; //* Stack of recycled entries for later reuse.
+
+	/** 
+	 * @brief Pointer to a mutex to prevent collisions between threads.
+	 * @note Implemented as a pointer so isEmpty() can be const.
+	 */
+	ThreadMutex * m_mutex;
+
+	//* Serial number for marks
+	unsigned int m_markCount;
+  };
+
+}
+
+#endif // VALUE_QUEUE_HH
