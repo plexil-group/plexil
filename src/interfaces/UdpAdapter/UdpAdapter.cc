54abe6dece8be1e37cc81abed8519e0d5c2d4145
Christian J. Plaunt <christian.j.plaunt@nasa.gov>
2011-06-30 20:56:46 +0000

    First pass at UDP Adapter scafolding
    git-svn-id: https://svn.code.sf.net/p/plexil/code/trunk@2190 d2be67b0-814c-0410-949f-d215b6b29573

---
+++ src/interfaces/UdpAdapter/UdpAdapter.cc
@@ -0,0 +1,173 @@
+/* Copyright (c) 2006-2008, Universities Space Research Association (USRA).
+*  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Universities Space Research Association nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "UdpAdapter.hh"
+
+#include "Debug.hh"
+#include "CoreExpressions.hh"
+#include "AdapterExecInterface.hh"
+#include "Node.hh"
+
+namespace PLEXIL
+{
+  /**
+   * @brief Base constructor.
+   */
+  UdpAdapter::UdpAdapter(AdapterExecInterface& execInterface) : InterfaceAdapter(execInterface)
+  {
+    debugMsg("UdpAdapter::UdpAdapter(execInterface)", " called");
+  }
+
+  /**
+   * @brief Constructor w/ configuration XML.
+   */
+  UdpAdapter::UdpAdapter(AdapterExecInterface& execInterface, const TiXmlElement* xml)
+    : InterfaceAdapter(execInterface, xml)
+  {
+    debugMsg("UdpAdapter::UdpAdapter(execInterface, xml)", " called");
+  }
+
+  /**
+   * @brief Destructor.
+   */
+  UdpAdapter::~UdpAdapter()
+  {
+    debugMsg("UdpAdapter::~UdpAdapter", " called");
+  }
+
+  /**
+   * @brief Initialize and register the udp adapter.
+   */
+  bool UdpAdapter::initialize()
+  {
+    debugMsg("UdpAdapter::initialize", " called");
+    this->registerAdapter();
+    //m_execInterface.defaultRegisterAdapter(getID());
+    //debugMsg("UdpAdapter::initialize", " done");
+    return true;
+  }
+
+  /**
+   * @brief Starts the adaptor, possibly using its configuration data.  
+   * @return true if successful, false otherwise.
+   */
+  bool UdpAdapter::start()
+  {
+    debugMsg("UdpAdapter::start()", " called");
+    return true;
+  }
+
+  /**
+   * @brief Stops the adaptor.  
+   * @return true if successful, false otherwise.
+   */
+  bool UdpAdapter::stop()
+  {
+    debugMsg("UdpAdapter::stop()", " called");
+    return true;
+  }
+
+  /**
+   * @brief Resets the adaptor.  
+   * @return true if successful, false otherwise.
+   */
+  bool UdpAdapter::reset()
+  {
+    debugMsg("UdpAdapter::reset()", " called");
+    return true;
+  }
+
+  /**
+   * @brief Shuts down the adaptor, releasing any of its resources.
+   * @return true if successful, false otherwise.
+   */
+  bool UdpAdapter::shutdown()
+  {
+    debugMsg("UdpAdapter::shutdown()", " called");
+    return true;
+  }
+
+  void UdpAdapter::registerChangeLookup(const LookupKey& /* uniqueId */,
+                                        const StateKey& /* stateKey */,
+                                        const std::vector<double>& /* tolerances */)
+  {
+    debugMsg("UdpAdapter::registerChangeLookup(Lookupkey, Statekey, tolerances)", " called");
+    debugMsg("ExternalInterface:udp", " registerChangeLookup called");
+  }
+
+  void UdpAdapter::unregisterChangeLookup(const LookupKey& /* uniqueId */)
+  {
+    debugMsg("UdpAdapter::unregisterChangeLookup(LookupKey)", " called");
+    debugMsg("ExternalInterface:udp", " unregisterChangeLookup called");
+  }
+
+  void UdpAdapter::lookupNow(const StateKey& /* key */,
+                             std::vector<double>& dest)
+  {
+    debugMsg("UdpAdapter::lookupNow(StateKey, dest)", " called");
+    debugMsg("ExternalInterface:udp", " lookupNow called; returning UNKNOWN");
+    dest[0] = Expression::UNKNOWN();
+  }
+
+  void UdpAdapter::sendPlannerUpdate(const NodeId& node,
+                                     const std::map<double, double>& /* valuePairs */,
+                                     ExpressionId ack)
+  {
+    debugMsg("UdpAdapter::sendPlannerUpdate(node, valuePairs, ack)", " called");
+    debugMsg("ExternalInterface:udp", " sendPlannerUpdate called");
+
+    // acknowledge updates
+    debugMsg("ExternalInterface:udp",
+	     " faking acknowledgment of update node '"
+	     << node->getNodeId().toString()
+	     << "'");
+    m_execInterface.handleValueChange(ack, BooleanVariable::TRUE());
+    m_execInterface.notifyOfExternalEvent();
+  }
+
+  void UdpAdapter::executeCommand(const LabelStr& /* name */,
+                                  const std::list<double>& /* args */,
+                                  ExpressionId /* dest */,
+                                  ExpressionId ack)
+  {
+    debugMsg("UdpAdapter::executeCommand(name, args, dest, ack)", " called");
+    debugMsg("ExternalInterface:udp", " executeCommand called");
+    m_execInterface.handleValueChange(ack, CommandHandleVariable::COMMAND_SENT_TO_SYSTEM());
+    m_execInterface.notifyOfExternalEvent();
+  }
+
+  //abort the given command with the given arguments.  store the abort-complete into dest
+  void UdpAdapter::invokeAbort(const LabelStr& /* name */, 
+                               const std::list<double>& /* args */, 
+                               ExpressionId /* dest */,
+                               ExpressionId ack)
+  {
+    debugMsg("UdpAdapter::invokeAbort(name, args, dest, ack)", " called");
+    debugMsg("ExternalInterface:udp", " invokeAbort called");
+    m_execInterface.handleValueChange(ack, BooleanVariable::TRUE());
+    m_execInterface.notifyOfExternalEvent();
+  }
+}
---
+++ src/interfaces/UdpAdapter/UdpAdapter.hh
@@ -0,0 +1,128 @@
+/* Copyright (c) 2006-2008, Universities Space Research Association (USRA).
+*  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Universities Space Research Association nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef UDP_ADAPTER_H
+#define UDP_ADAPTER_H
+
+#include "InterfaceAdapter.hh"
+
+//
+// Forward references w/o namespace
+//
+
+class TiXmlElement;
+
+namespace PLEXIL
+{
+  /*!
+    \brief A UDP InterfaceAdapter for testing purposes
+  */
+  class UdpAdapter : public InterfaceAdapter
+  {
+  public:
+
+    /**
+     * @brief Base constructor.
+     */
+    UdpAdapter(AdapterExecInterface& execInterface);
+
+    /**
+     * @brief Constructor w/ configuration XML.
+     */
+    UdpAdapter(AdapterExecInterface& execInterface,
+                 const TiXmlElement* xml);
+
+    /**
+     * @brief Destructor.
+     */
+    ~UdpAdapter();
+
+    /**
+     * @brief Initialize and register the adapter, possibly using its configuration data.
+     * @return true if successful, false otherwise.
+     */
+    bool initialize();
+
+    /**
+     * @brief Starts the adapter, possibly using its configuration data.  
+     * @return true if successful, false otherwise.
+     */
+    bool start();
+
+    /**
+     * @brief Stops the adapter.  
+     * @return true if successful, false otherwise.
+     */
+    bool stop();
+
+    /**
+     * @brief Resets the adapter.  
+     * @return true if successful, false otherwise.
+     */
+    bool reset();
+
+    /**
+     * @brief Shuts down the adapter, releasing any of its resources.
+     * @return true if successful, false otherwise.
+     */
+    bool shutdown();
+
+    virtual void registerChangeLookup(const LookupKey& uniqueId,
+				      const StateKey& stateKey,
+				      const std::vector<double>& tolerances);
+
+    virtual void unregisterChangeLookup(const LookupKey& uniqueId);
+
+    void lookupNow(const StateKey& stateKey,
+		   std::vector<double>& dest);
+
+    void sendPlannerUpdate(const NodeId& node,
+			   const std::map<double, double>& valuePairs,
+			   ExpressionId ack);
+
+    // executes a command with the given arguments
+    void executeCommand(const LabelStr& name,
+			const std::list<double>& args,
+			ExpressionId dest,
+			ExpressionId ack);
+
+    //abort the given command with the given arguments.  store the abort-complete into ack
+    void invokeAbort(const LabelStr& name, 
+		     const std::list<double>& args, 
+		     ExpressionId dest,
+		     ExpressionId ack);
+    
+  private:
+    // deliberately unimplemented
+    UdpAdapter();
+    UdpAdapter(const UdpAdapter &);
+    UdpAdapter& operator=(const UdpAdapter &);
+
+  };
+
+}
+
+#endif // UDP_ADAPTER_H
