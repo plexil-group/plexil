76be0a91ffc65f82a3238499d54897dc1ff14371
Christian J. Plaunt <christian.j.plaunt@nasa.gov>
2011-07-01 20:19:16 +0000

    cleaned up and simplified several things; moved initUdpAdapter into UdpAdapter.{hh,cc}; starting to work on XML config message definitions; capturing useful stable state
    git-svn-id: https://svn.code.sf.net/p/plexil/code/trunk@2194 d2be67b0-814c-0410-949f-d215b6b29573

--- src/interfaces/UdpAdapter/UdpAdapter.cc
+++ src/interfaces/UdpAdapter/UdpAdapter.cc
@@ -1,27 +1,5 @@
-/* Copyright (c) 2006-2008, Universities Space Research Association (USRA).
-*  All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in the
-*       documentation and/or other materials provided with the distribution.
-*     * Neither the name of the Universities Space Research Association nor the
-*       names of its contributors may be used to endorse or promote products
-*       derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/* -*- Mode: c++ -*-
+   File: plexilsrc/interfaces/UdpAdapter/UdpAdapter.cc
 */
 
 #include "UdpAdapter.hh"
@@ -29,42 +7,50 @@
 #include "CoreExpressions.hh"   // BooleanVariable, etc.
 #include "Node.hh"              // struct PLEXIL::Node
 #include "AdapterExecInterface.hh"
+#include "AdapterFactory.hh"    // initUdpAdapter
+
 //#include "Error.hh"
 //#include "PlexilXmlParser.h"
+//#include "ExecListenerFactory.hh"
+
+// for UdpAdapter constructor
+#ifndef TIXML_USE_STL
+#define TIXML_USE_STL
+#endif
+#include "tinyxml.h"
 
 namespace PLEXIL
 {
-  /**
-   * @brief Base constructor.
-   */
-  UdpAdapter::UdpAdapter(AdapterExecInterface& execInterface) : InterfaceAdapter(execInterface)
-  {
-    debugMsg("UdpAdapter::UdpAdapter(execInterface)", " called");
-  }
+  // Constructor
+  //UdpAdapter::UdpAdapter(AdapterExecInterface& execInterface) : InterfaceAdapter(execInterface)
+  //{
+  //  debugMsg("UdpAdapter::UdpAdapter(execInterface)", " called");
+  //}
 
-  /**
-   * @brief Constructor w/ configuration XML.
-   */
+  // Constructor
   UdpAdapter::UdpAdapter(AdapterExecInterface& execInterface, const TiXmlElement* xml)
     : InterfaceAdapter(execInterface, xml)
   {
-    debugMsg("UdpAdapter::UdpAdapter(execInterface, xml)", " called");
+    assertTrue(xml != NULL, "XML config file not found in UdpAdapter::UdpAdapter constructor");
+    //debugMsg("UdpAdapter::UdpAdapter", " Using " << xml->Attribute("AdapterType"));
+    debugMsg("UdpAdapter::UdpAdapter", " Using " << xml->Document());
   }
 
-  /**
-   * @brief Destructor.
-   */
+  // Destructor
   UdpAdapter::~UdpAdapter()
   {
     debugMsg("UdpAdapter::~UdpAdapter", " called");
   }
 
-  /**
-   * @brief Initialize and register the udp adapter.
-   */
+  // Initialize
   bool UdpAdapter::initialize()
   {
     debugMsg("UdpAdapter::initialize", " called");
+
+    // Parse the message definitions in the XML configuration
+    const TiXmlElement* xml = this->getXml();
+    debugMsg("UdpAdapter::initialize", " xml = " << *xml);
+
     //this->registerAdapter();
     //m_execInterface.defaultRegisterAdapter(getId());
     m_execInterface.registerCommandInterface(LabelStr(SEND_MESSAGE_COMMAND()), getId());
@@ -72,40 +58,28 @@ namespace PLEXIL
     return true;
   }
 
-  /**
-   * @brief Starts the adaptor, possibly using its configuration data.  
-   * @return true if successful, false otherwise.
-   */
+  // Start method
   bool UdpAdapter::start()
   {
     debugMsg("UdpAdapter::start()", " called");
     return true;
   }
 
-  /**
-   * @brief Stops the adaptor.  
-   * @return true if successful, false otherwise.
-   */
+  // Stop method
   bool UdpAdapter::stop()
   {
     debugMsg("UdpAdapter::stop()", " called");
     return true;
   }
 
-  /**
-   * @brief Resets the adaptor.  
-   * @return true if successful, false otherwise.
-   */
+  // Reset method
   bool UdpAdapter::reset()
   {
     debugMsg("UdpAdapter::reset()", " called");
     return true;
   }
 
-  /**
-   * @brief Shuts down the adaptor, releasing any of its resources.
-   * @return true if successful, false otherwise.
-   */
+  // Shutdown method
   bool UdpAdapter::shutdown()
   {
     debugMsg("UdpAdapter::shutdown()", " called");
@@ -150,10 +124,10 @@ namespace PLEXIL
     m_execInterface.notifyOfExternalEvent();
   }
 
+  // Execute a Plexil Command
   void UdpAdapter::executeCommand(const LabelStr& name, const std::list<double>& args, ExpressionId dest, ExpressionId ack)
   {
     debugMsg("UdpAdapter::executeCommand", " called for " << name.c_str());
-    //debugMsg("ExternalInterface:udp", " executeCommand called");
     if (name == SEND_MESSAGE_COMMAND())
       executeSendMessageCommand(args, dest, ack);
     m_execInterface.handleValueChange(ack, CommandHandleVariable::COMMAND_SENT_TO_SYSTEM());
@@ -176,9 +150,7 @@ namespace PLEXIL
   // Implementation methods
   //
 
-  /**
-   * @brief handles SEND_MESSAGE_COMMAND commands from the exec
-   */
+  // SEND_MESSAGE_COMMAND
   void UdpAdapter::executeSendMessageCommand(const std::list<double>& args, ExpressionId /* dest */, ExpressionId ack)
   {
     // Check for one argument, the message
@@ -194,5 +166,14 @@ namespace PLEXIL
     m_execInterface.handleValueChange(ack, CommandHandleVariable::COMMAND_SUCCESS().getKey());
     m_execInterface.notifyOfExternalEvent();
     debugMsg("UdpAdapter::executeSendMessageCommand", " message \"" << theMessage.c_str() << "\" sent.");
+  }
+}
+
+// Register the UdpAdapter
+extern "C"
+{
+  void initUdpAdapter()
+  {
+    REGISTER_ADAPTER(PLEXIL::UdpAdapter, "UdpAdapter");
   }
 }
--- src/interfaces/UdpAdapter/UdpAdapter.hh
+++ src/interfaces/UdpAdapter/UdpAdapter.hh
@@ -1,39 +1,11 @@
-/* Copyright (c) 2006-2008, Universities Space Research Association (USRA).
-*  All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*     * Redistributions of source code must retain the above copyright
-*       notice, this list of conditions and the following disclaimer.
-*     * Redistributions in binary form must reproduce the above copyright
-*       notice, this list of conditions and the following disclaimer in the
-*       documentation and/or other materials provided with the distribution.
-*     * Neither the name of the Universities Space Research Association nor the
-*       names of its contributors may be used to endorse or promote products
-*       derived from this software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+/* -*- Mode: c++ -*-
+ * File: plexil/src/interfaces/UdpAdapter/UdpAdapter.hh
+ */
 
 #include "InterfaceAdapter.hh"
 #include "LabelStr.hh"
-//#include "ExecListener.hh"
-//#include "AdapterExecInterface.hh"
-//#include "ExecListener.hh"
 
-//
 // Forward references w/o namespace
-//
-
 class TiXmlElement;
 
 namespace PLEXIL
@@ -50,62 +22,25 @@ namespace PLEXIL
     //DECLARE_STATIC_CLASS_CONST(LabelStr, SEND_RETURN_VALUE_COMMAND, "SendReturnValue")
     //DECLARE_STATIC_CLASS_CONST(LabelStr, UPDATE_LOOKUP_COMMAND, "UpdateLookup")
 
-    /**
-     * @brief Base constructor.
-     */
-    UdpAdapter(AdapterExecInterface& execInterface);
+    // Constructor
+    //UdpAdapter(AdapterExecInterface& execInterface);
 
-    /**
-     * @brief Constructor w/ configuration XML.
-     */
+    // Constructor/Destructor
     UdpAdapter(AdapterExecInterface& execInterface, const TiXmlElement* xml);
-
-    /**
-     * @brief Destructor.
-     */
     virtual ~UdpAdapter();
 
-    /**
-     * @brief Initialize and register the adapter, possibly using its configuration data.
-     * @return true if successful, false otherwise.
-     */
-    virtual bool initialize();
-
-    /**
-     * @brief Starts the adapter, possibly using its configuration data.  
-     * @return true if successful, false otherwise.
-     */
-    virtual bool start();
-
-    /**
-     * @brief Stops the adapter.  
-     * @return true if successful, false otherwise.
-     */
-    virtual bool stop();
-
-    /**
-     * @brief Resets the adapter.  
-     * @return true if successful, false otherwise.
-     */
-    virtual bool reset();
-
-    /**
-     * @brief Shuts down the adapter, releasing any of its resources.
-     * @return true if successful, false otherwise.
-     */
-    virtual bool shutdown();
-
-    virtual void registerChangeLookup(const LookupKey& uniqueId, const StateKey& stateKey, const std::vector<double>& tolerances);
-
-    virtual void unregisterChangeLookup(const LookupKey& uniqueId);
-
-    virtual void lookupNow(const StateKey& stateKey, std::vector<double>& dest);
-
-    virtual void sendPlannerUpdate(const NodeId& node, const std::map<double, double>& valuePairs, ExpressionId ack);
-
+    // InterfaceAdapter API
+    bool initialize();
+    bool start();
+    bool stop();
+    bool reset();
+    bool shutdown();
+    void registerChangeLookup(const LookupKey& uniqueId, const StateKey& stateKey, const std::vector<double>& tolerances);
+    void unregisterChangeLookup(const LookupKey& uniqueId);
+    void lookupNow(const StateKey& stateKey, std::vector<double>& dest);
+    void sendPlannerUpdate(const NodeId& node, const std::map<double, double>& valuePairs, ExpressionId ack);
     // executes a command with the given arguments
-    virtual void executeCommand(const LabelStr& name, const std::list<double>& args, ExpressionId dest, ExpressionId ack);
-
+    void executeCommand(const LabelStr& name, const std::list<double>& args, ExpressionId dest, ExpressionId ack);
     //abort the given command with the given arguments.  store the abort-complete into ack
     void invokeAbort(const LabelStr& name, const std::list<double>& args, ExpressionId dest, ExpressionId ack);
 
@@ -125,4 +60,9 @@ namespace PLEXIL
     void executeSendMessageCommand(const std::list<double>& args, ExpressionId dest, ExpressionId ack);
 
   };
+}
+
+extern "C"
+{
+  void initUdpAdapter();
 }
