9db6f4b367e05aad932f630f5ada95217de6dcc0
Christian J. Plaunt <christian.j.plaunt@nasa.gov>
2011-09-12 19:15:48 +0000

    added copyright statements (copies from IpcAdapter) to everything in preparation for Source Forge; added range tests for 2 bit integers to buildUdpBuffer;
    git-svn-id: https://svn.code.sf.net/p/plexil/code/trunk@2459 d2be67b0-814c-0410-949f-d215b6b29573

--- src/interfaces/UdpAdapter/UdpAdapter.cc
+++ src/interfaces/UdpAdapter/UdpAdapter.cc
@@ -1,6 +1,28 @@
-/* -*- Mode: c++ -*-
-   File: plexilsrc/interfaces/UdpAdapter/UdpAdapter.cc
-*/
+/* Copyright (c) 2006-2009, Universities Space Research Association (USRA).
+ *  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Universities Space Research Association nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "UdpAdapter.hh"
 #include "Debug.hh"             // debugMsg
@@ -747,10 +769,16 @@ namespace PLEXIL
         if (type.compare("int") == 0)
           {
             assertTrueMsg((len==2 || len==4), "buildUdpBuffer: Integers must be 2 or 4 bytes, not " << len);
-            // Large ints are caught in Expression.cc:337
+            // Large ints (> 32 bits) are caught in Expression.cc:337
             if (debug) std::cout << len << " byte int starting at buffer[" << start_index << "]: " << (int)plexil_val;
             if (len==2)
-              encode_short_int((int)plexil_val, buffer, start_index);
+              {
+                int temp = (int)plexil_val;
+                assertTrueMsg(SHRT_MIN <= temp && temp <= SHRT_MAX,
+                              "buildUdpBuffer: 2 bytes integers must be between " << SHRT_MIN << " and " << SHRT_MAX
+                              << ", " << temp << " is not");
+                encode_short_int(temp, buffer, start_index);
+              }
             else
               encode_long_int((int)plexil_val, buffer, start_index);
             start_index += len;
@@ -767,7 +795,15 @@ namespace PLEXIL
             for (int i = 0 ; i < size ; i++)
               {
                 int temp = (int)array[i];
-                (len==2) ? encode_short_int(temp, buffer, start_index) : encode_long_int(temp, buffer, start_index);
+                if (len==2)
+                  {
+                    assertTrueMsg(SHRT_MIN <= temp && temp <= SHRT_MAX,
+                                  "buildUdpBuffer: 2 bytes integers must be between " << SHRT_MIN << " and " << SHRT_MAX
+                                  << ", " << temp << " is not");
+                    encode_short_int(temp, buffer, start_index);
+                  }
+                else
+                  encode_long_int(temp, buffer, start_index);
                 start_index += len;
               }
           }
@@ -785,7 +821,6 @@ namespace PLEXIL
               {
                 float temp = array[i];
                 assertTrueMsg(FLT_MIN <= temp <= FLT_MAX,
-                              //(FLT_MIN <= temp) && (FLT_MAX >= temp),
                               "buildUdpBuffer: Reals (floats) must be between " << FLT_MIN << " and " << FLT_MAX <<
                               ", " << temp << " is not");
                 encode_float(temp, buffer, start_index);
