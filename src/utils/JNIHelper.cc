50253a63a2ba73a7acd611878b5f5a6ef4be5563
Chuck Fry <chuck.fry@nasa.gov>
2012-01-31 19:23:21 +0000

    Remove helper macro, which wasn't working anyway
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/exec-refactoring-2011@2662 d2be67b0-814c-0410-949f-d215b6b29573

--- src/utils/JNIHelper.cc
+++ src/utils/JNIHelper.cc
@@ -26,14 +26,7 @@
 
 #include "JNIHelper.hh"
 #include "Debug.hh"
-#include <cassert> // for assert()
-
-// Local helper macro for use inside methods
-#define ENSURE_CLASS_METHOD_ID(varref, methodname, signature)			\
-  { if (varref == NULL)													\
-	  varref = m_env->GetMethodID(getClassClass(), methodname, signature); \
-	assert(varref != NULL);												\
-  }
+#include "Error.hh"
 
 // Initialization for the static instance pointer
 JNIHelper* JNIHelper::s_instance = NULL;
@@ -42,7 +35,9 @@ JNIHelper::JNIHelper(JNIEnv* env, jobject java_this)
   : m_env(env),
 	m_java_this(java_this),
 	m_previous(s_instance),
-	m_classClass(NULL)
+	m_classClass(NULL),
+	m_isArrayMethod(NULL),
+	m_getNameMethod(NULL)
 {
   debugMsg("JNIHelper",
 		   " constructor, "
@@ -52,7 +47,7 @@ JNIHelper::JNIHelper(JNIEnv* env, jobject java_this)
 
 JNIHelper::~JNIHelper()
 {
-  assert(s_instance == this);
+  assertTrue(s_instance == this, "JNIHelper destructor: this != s_instance");
 
   // Restore static instance pointer from reentrant calls
   debugMsg("JNIHelper",
@@ -80,8 +75,9 @@ jobject JNIHelper::getJavaThis()
 jclass JNIHelper::getClassClass()
 {
   if (m_classClass == NULL) {
-	debugMsg("JNIHelper:getClassClass", " fetching Class class from JNI");
+	debugMsg("JNIHelper:getClassClass", " fetching Class class");
 	m_classClass = m_env->FindClass("java/lang/Class");
+	assertTrue(m_classClass != NULL, "JNIHelper::getClassClass failed");
   }
   return m_classClass;
 }
@@ -92,6 +88,7 @@ jclass JNIHelper::getClassClass()
 bool JNIHelper::isArray(jobject object)
 {
   jclass objectClass = m_env->GetObjectClass(object);
+  assertTrue(objectClass != NULL, "JNIHelper::isArray: GetObjectClass() returned NULL");
   bool result = isArrayClass(objectClass);
   m_env->DeleteLocalRef(objectClass);
   return result;
@@ -102,8 +99,15 @@ bool JNIHelper::isArray(jobject object)
  */
 bool JNIHelper::isArrayClass(jclass klass)
 {
-  ENSURE_CLASS_METHOD_ID(m_isArrayMethod, "isArray", "()Z");
-  return m_env->CallBooleanMethod(klass, m_isArrayMethod);
+  if (m_isArrayMethod == NULL) {
+	debugMsg("JNIHelper:isArrayClass", " fetching Class.isArray() method");
+	m_isArrayMethod = m_env->GetMethodID(getClassClass(), "isArray", "()Z");
+	assertTrue(m_isArrayMethod != NULL, "JNIHelper::isArrayClass: Failed to fetch Class.isArray() method");
+  }
+  debugMsg("JNIHelper:isArrayClass", " calling Class.isArray()");
+  bool result = m_env->CallBooleanMethod(klass, m_isArrayMethod);
+  debugMsg("JNIHelper:isArrayClass", " returning " << (result ? "true" : "false"));
+  return result;
 }
 
 
@@ -114,7 +118,11 @@ bool JNIHelper::isArrayClass(jclass klass)
  */
 char* JNIHelper::getClassName(jclass klass)
 {
-  ENSURE_CLASS_METHOD_ID(m_getNameMethod, "getName", "()Ljava/lang/String;");
+  if (m_getNameMethod == NULL) {
+	debugMsg("JNIHelper:getClassName", " fetching Class.getName() method");
+	m_getNameMethod = m_env->GetMethodID(getClassClass(), "getName", "()Ljava/lang/String;");
+	assertTrue(m_getNameMethod != NULL, "JNIHelper::getClassName: Failed to fetch Class.getName() method");
+  }
   jstring name = (jstring) m_env->CallObjectMethod(klass, m_getNameMethod);
   if (name == NULL) 
 	return NULL;
