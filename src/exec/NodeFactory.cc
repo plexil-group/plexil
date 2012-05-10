14ed565beee6c625734e581b81588a8ae8972f7b
Chuck Fry <chuck.fry@nasa.gov>
2012-05-10 00:07:37 +0000

    Get rid of condition value args for alternate call to createNode(), as they were only ever populated with lists of false
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/exit-condition@2767 d2be67b0-814c-0410-949f-d215b6b29573

--- src/exec/NodeFactory.cc
+++ src/exec/NodeFactory.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2011, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2012, Universities Space Research Association (USRA).
 *  All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -34,109 +34,87 @@
 namespace PLEXIL
 {
   NodeFactory::NodeFactory(PlexilNodeType nodeType)
-	: m_nodeType(nodeType)
+    : m_nodeType(nodeType)
   {
-	checkError(factoryMap()[nodeType] == NULL,
-			   "A NodeFactory is already registered for node type " << PlexilParser::nodeTypeString(nodeType));
-	factoryMap()[nodeType] = this;
+    checkError(factoryMap()[nodeType] == NULL,
+               "A NodeFactory is already registered for node type " << PlexilParser::nodeTypeString(nodeType));
+    factoryMap()[nodeType] = this;
   }
 
   NodeFactory::~NodeFactory()
   {
-	factoryMap()[m_nodeType] = NULL;
+    factoryMap()[m_nodeType] = NULL;
   }
 
   NodeFactory** NodeFactory::factoryMap()
   {
-	static NodeFactory* *sl_factories = NULL;
-	if (sl_factories == NULL) {
-	  sl_factories = new NodeFactory*[NodeType_error];
-	  for (size_t i = 0; i < NodeType_error; i++)
-		sl_factories[i] = NULL;
-	}
-	return sl_factories;
+    static NodeFactory* *sl_factories = NULL;
+    if (sl_factories == NULL) {
+      sl_factories = new NodeFactory*[NodeType_error];
+      for (size_t i = 0; i < NodeType_error; i++)
+        sl_factories[i] = NULL;
+    }
+    return sl_factories;
   }
 
   /**
    * @brief Primary factory method.
    */
   NodeId NodeFactory::createNode(const PlexilNodeId& nodeProto, 
-								 const ExecConnectorId& exec, 
-								 const NodeId& parent)
+                                 const ExecConnectorId& exec, 
+                                 const NodeId& parent)
   {
-	checkError(nodeProto.isValid(), "Invalid PlexilNodeId");
-	PlexilNodeType nodeType = nodeProto->nodeType();
-	checkError(nodeType > NodeType_uninitialized
-			   && nodeType < NodeType_error,
-			   "Invalid node type value " << nodeType);
-	ensureNodeFactoriesRegistered();
-	NodeFactory* factory = factoryMap()[nodeType];
-	checkError(factory != NULL, 
-			   "No NodeFactory registered for node type " << PlexilParser::nodeTypeString(nodeType));
-	NodeId result = factory->create(nodeProto, exec, parent);
-	// common post process here?
-	return result;
+    checkError(nodeProto.isValid(), "Invalid PlexilNodeId");
+    PlexilNodeType nodeType = nodeProto->nodeType();
+    checkError(nodeType > NodeType_uninitialized
+               && nodeType < NodeType_error,
+               "Invalid node type value " << nodeType);
+    ensureNodeFactoriesRegistered();
+    NodeFactory* factory = factoryMap()[nodeType];
+    checkError(factory != NULL, 
+               "No NodeFactory registered for node type " << PlexilParser::nodeTypeString(nodeType));
+    NodeId result = factory->create(nodeProto, exec, parent);
+    // common post process here?
+    return result;
   }
 
   /**
    * @brief Alternate factory method.  Used only by Exec test module.
    */
   NodeId NodeFactory::createNode(const LabelStr& type, 
-								 const LabelStr& name, 
-								 const NodeState state,
-								 const bool skip,
-								 const bool start,
-								 const bool pre,
-								 const bool invariant, 
-								 const bool post,
-								 const bool end,
-								 const bool repeat,
-								 const bool ancestorInvariant,
-								 const bool ancestorEnd,
-								 const bool parentExecuting,
-								 const bool childrenFinished,
-								 const bool commandAbort,
-								 const bool parentWaiting,
-								 const bool parentFinished,
-								 const bool cmdHdlRcvdCondition,
-								 const ExecConnectorId& exec,
-								 const NodeId& parent)
+                                 const LabelStr& name, 
+                                 const NodeState state,
+                                 const ExecConnectorId& exec,
+                                 const NodeId& parent)
   {
-	PlexilNodeType nodeType = PlexilParser::parseNodeType(type.toString());
-	checkError(nodeType > NodeType_uninitialized
-			   && nodeType < NodeType_error,
-			   "Invalid node type string " << type.toString());
-	ensureNodeFactoriesRegistered();
-	NodeFactory* factory = factoryMap()[nodeType];
-	checkError(factory != NULL, 
-			   "No NodeFactory registered for node type " << type.toString());
-	NodeId result = factory->create(type, name, state,
-									skip, start, pre,
-									invariant, post, end, repeat,
-									ancestorInvariant, ancestorEnd,
-									parentExecuting, childrenFinished,
-									commandAbort, parentWaiting, parentFinished,
-									cmdHdlRcvdCondition,
-									exec, parent);
-	// common post process here?
-	return result;
+    PlexilNodeType nodeType = PlexilParser::parseNodeType(type.toString());
+    checkError(nodeType > NodeType_uninitialized
+               && nodeType < NodeType_error,
+               "Invalid node type string " << type.toString());
+    ensureNodeFactoriesRegistered();
+    NodeFactory* factory = factoryMap()[nodeType];
+    checkError(factory != NULL, 
+               "No NodeFactory registered for node type " << type.toString());
+    NodeId result = factory->create(type, name, state, exec, parent);
+    // common post process here?
+    return result;
   }
 
 #define REGISTER_NODE_FACTORY(CLASS,NODE_TYPE) { new PLEXIL::ConcreteNodeFactory<CLASS>(NODE_TYPE); }
 
   void NodeFactory::ensureNodeFactoriesRegistered()
   {
-	static bool sl_registered = false;
-	if (sl_registered)
-	  return;
+    static bool sl_registered = false;
+    if (sl_registered)
+      return;
 
-	REGISTER_NODE_FACTORY(ListNode, NodeType_NodeList);
-	REGISTER_NODE_FACTORY(CommandNode, NodeType_Command);
-	REGISTER_NODE_FACTORY(AssignmentNode, NodeType_Assignment);
-	REGISTER_NODE_FACTORY(UpdateNode, NodeType_Update);
-	REGISTER_NODE_FACTORY(Node, NodeType_Empty);
-	REGISTER_NODE_FACTORY(LibraryCallNode, NodeType_LibraryNodeCall);
-	sl_registered = true;
+    REGISTER_NODE_FACTORY(ListNode, NodeType_NodeList);
+    REGISTER_NODE_FACTORY(CommandNode, NodeType_Command);
+    REGISTER_NODE_FACTORY(AssignmentNode, NodeType_Assignment);
+    REGISTER_NODE_FACTORY(UpdateNode, NodeType_Update);
+    REGISTER_NODE_FACTORY(Node, NodeType_Empty);
+    REGISTER_NODE_FACTORY(LibraryCallNode, NodeType_LibraryNodeCall);
+    sl_registered = true;
   }
 
 }
