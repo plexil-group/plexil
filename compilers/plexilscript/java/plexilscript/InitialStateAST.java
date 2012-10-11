1b5909282d507b899865678309b96bcbdd680bf1
Chuck Fry <chuck.fry@nasa.gov>
2012-10-11 22:34:13 +0000

    Added back Plexilscript compiler from trunk
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-3@2992 d2be67b0-814c-0410-949f-d215b6b29573

---
+++ compilers/plexilscript/java/plexilscript/InitialStateAST.java
@@ -0,0 +1,24 @@
+
+//
+// InitialStateAST
+//
+
+package plexilscript;
+
+import java.util.List;
+
+public class InitialStateAST implements ElementAST {
+    List<ElementAST> es;
+
+    public InitialStateAST (List<ElementAST> es0) {
+	es = es0;
+    }
+
+    public void print () {
+	System.out.printf ("  <InitialState>\n");
+	for (ElementAST e : es)
+	    e.print ();
+	System.out.printf ("  </InitialState>\n");
+    }
+}
+
---
+++ compilers/plexilscript/java/plexilscript/ScriptAST.java
@@ -0,0 +1,24 @@
+
+//
+// ScriptAST
+//
+
+package plexilscript;
+
+import java.util.List;
+
+public class ScriptAST implements ElementAST {
+    List<ElementAST> es;
+
+    public ScriptAST (List<ElementAST> es0) {
+	es = es0;
+    }
+
+    public void print () {
+	System.out.printf ("  <Script>\n");
+	for (ElementAST e : es)
+	    e.print ();
+	System.out.printf ("  </Script>\n");
+    }
+}
+
