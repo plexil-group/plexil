9bedd4c12af52c366806832ac9cc8139aff98f25
Chuck Fry <chuck.fry@nasa.gov>
2014-02-10 19:27:12 +0000

    Replace NULL with 0, to avoid including another header file
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-3@3311 d2be67b0-814c-0410-949f-d215b6b29573

--- src/utils/hash-functions.c
+++ src/utils/hash-functions.c
@@ -79,7 +79,7 @@ uint32_t PaulHsiehHash (const char * data, int len) {
   uint32_t hash = len, tmp;
   int rem;
 
-  if (len <= 0 || data == NULL) return 0;
+  if (len <= 0 || data == 0) return 0;
 
   rem = len & 3;
   len >>= 2;
