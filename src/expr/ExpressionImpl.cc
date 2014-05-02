875f3f0d89720e24ab9160f409aeed43a1f13844
Chuck Fry <chuck.fry@nasa.gov>
2014-05-02 21:51:15 +0000

    Add copyright notice (oops)
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-3-new-exp@3443 d2be67b0-814c-0410-949f-d215b6b29573

--- src/expr/ExpressionImpl.cc
+++ src/expr/ExpressionImpl.cc
@@ -1,8 +1,60 @@
+/* Copyright (c) 2006-2014, Universities Space Research Association (USRA).
+*  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Universities Space Research Association nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include "ExpressionImpl.hh"
 #include "Error.hh"
 
 namespace PLEXIL
 {
+  /**
+   * @brief Query whether the expression's value is known.
+   * @return True if known, false otherwise.
+   */
+  template <typename T>
+  bool ExpressionImpl<T>::isKnown() const
+  {
+    T dummy;
+    return this->getValueImpl(dummy);
+  }
+
+  /**
+   * @brief Print the expression's value to the given stream.
+   * @param s The output stream.
+   * @note Default method, specializations should be implemented as appropriate.
+   */
+  template <typename T>
+  void ExpressionImpl<T>::printValue(std::ostream& s) const
+  {
+    T temp;
+    if (this->getValueImpl(temp))
+      s << temp;
+    else
+      s << "UNKNOWN";
+  }
+
   template <typename T>
   template <typename U>
   bool ExpressionImpl<T>::getValueImpl(U &result) const
--- src/expr/ExpressionImpl.hh
+++ src/expr/ExpressionImpl.hh
@@ -1,3 +1,29 @@
+/* Copyright (c) 2006-2014, Universities Space Research Association (USRA).
+*  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Universities Space Research Association nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #ifndef PLEXIL_EXPRESSION_IMPL_HH
 #define PLEXIL_EXPRESSION_IMPL_HH
 
@@ -51,6 +77,24 @@ namespace PLEXIL
   class ExpressionImpl : public ExpressionWrapper<ExpressionImpl<T> >
   {
   public:
+    /**
+     * @brief Query whether the expression's value is known.
+     * @return True if known, false otherwise.
+     * @note Default method uses getValueImpl().
+     */
+    bool isKnown() const;
+
+	/**
+	 * @brief Print the expression's value to the given stream.
+	 * @param s The output stream.
+	 */
+    void printValue(std::ostream &) const; // may be overridden?
+
+    /**
+     * @brief Retrieve the value of this Expression in its native type.
+     * @param The appropriately typed place to put the result.
+     * @return True if known, false if unknown.
+     */
     virtual bool getValueImpl(T &result) const = 0; 
 
     // Conversion wrapper
