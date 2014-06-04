40d540e6f340ff2a9da38b94101e6a3757970dd0
Chuck Fry <chuck.fry@nasa.gov>
2014-06-04 06:59:28 +0000

    Convenience shell script for setting DYLD_LIBRARY_PATH
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-3-new-exp@3547 d2be67b0-814c-0410-949f-d215b6b29573

---
+++ src/setup.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+export PLEXIL_HOME=/Users/chucko/src/plexil-3-new-exp
+# Add to this as needed
+export DYLD_LIBRARY_PATH=$PLEXIL_HOME/src/third-party/pugixml/src/.libs:$PLEXIL_HOME/src/utils/.libs:$PLEXIL_HOME/src/expr/.libs:$PLEXIL_HOME/src/intfc/.libs:$PLEXIL_HOME/src/exec/.libs
