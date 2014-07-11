1595c27807e2fb3baa45cbda7948513931767553
Chuck Fry <chuck.fry@nasa.gov>
2014-07-11 21:18:23 +0000

    Recognize new format of node outcome
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-3-new-exp@3701 d2be67b0-814c-0410-949f-d215b6b29573

--- test/TestExec-regression-test/regression.pl
+++ test/TestExec-regression-test/regression.pl
@@ -95,8 +95,8 @@ close(F2);
                     if(@lines1[$i] =~ m/PlexilExec:printPlan/){
                          $k = 1;
                         if (@lines1[$i+1] =~ m/^$rootNode\{$/){
-                            if (@lines1[$i+3] =~ m/^ Outcome: $rootNode \((id_[0-9]{2,4}|ptr_0x[0-9a-fA-F]{2,16})\[a\]\((.*)\): outcome/){
-							  $outcome = $2;
+                            if (@lines1[$i+3] =~ m/^ Outcome: ([A-Z]*)$/){
+							  $outcome = $1;
                                 if ($outcome eq "SUCCESS"){
                                     print DIF "\nTEST PASSED: ", $testName, "\n";
                                     $i = 0;
