e55fd4a0dd531db39a3898718e4a4b416edd2e3e
Chuck Fry <chuck.fry@nasa.gov>
2014-08-18 23:39:33 +0000

    Add PLEXIL directories to PATH
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-4@3874 d2be67b0-814c-0410-949f-d215b6b29573

--- src/setup.sh
+++ src/setup.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env bash
 
-export PLEXIL_HOME=/Users/chucko/src/plexil-3-new-exp
+export PLEXIL_HOME=/Users/cfry/src/plexil-4
+
 # Add to this as needed
-export DYLD_LIBRARY_PATH=$PLEXIL_HOME/src/third-party/pugixml/src/.libs:$PLEXIL_HOME/src/utils/.libs:$PLEXIL_HOME/src/expr/.libs:$PLEXIL_HOME/src/intfc/.libs:$PLEXIL_HOME/src/exec/.libs:$PLEXIL_HOME/src/interfaces/PlanDebugListener/.libs:$PLEXIL_HOME/src/interfaces/LuvListener/.libs:$PLEXIL_HOME/src/interfaces/Sockets/.libs:$PLEXIL_HOME/src/apps/TestExec/.libs
+export DYLD_LIBRARY_PATH=$PLEXIL_HOME/src/third-party/pugixml/src/.libs:$PLEXIL_HOME/src/utils/.libs:$PLEXIL_HOME/src/expr/.libs:$PLEXIL_HOME/src/intfc/.libs:$PLEXIL_HOME/src/exec/.libs:$PLEXIL_HOME/src/interfaces/PlanDebugListener/.libs:$PLEXIL_HOME/src/interfaces/LuvListener/.libs:$PLEXIL_HOME/src/interfaces/Sockets/.libs:$PLEXIL_HOME/src/apps/TestExec/.libs:$PLEXIL_HOME/lib
+
+export PATH=$PATH:$PLEXIL_HOME/bin:$PLEXIL_HOME/scripts
+
