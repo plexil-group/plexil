9cd7380e2b1e12fbcaa5cbe6b32ec779fe1a5dda
Chuck Fry <chuck.fry@nasa.gov>
2014-10-03 00:13:51 +0000

    New; move all library node functionality in the parser to here
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-4@4011 d2be67b0-814c-0410-949f-d215b6b29573

---
+++ src/xml-parser/planLibrary.cc
@@ -0,0 +1,147 @@
+/* Copyright (c) 2006-2014, Universities Space Research Association (USRA).
+*  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Universities Space Research Association nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "planLibrary.hh"
+
+#include "Error.hh"
+#include "lifecycle-utils.h"
+#include "parsePlan.hh"
+#include "PlexilSchema.hh"
+
+
+#include <map>
+
+using pugi::xml_document;
+using pugi::xml_node;
+using std::map;
+using std::string;
+using std::vector;
+
+namespace PLEXIL
+{
+  //
+  // Static variables local to this file
+  //
+
+  // List of library directories to search
+  static vector<string> librarySearchPaths;
+
+  // Place to store library nodes
+  static map<string, xml_document *> libraryMap;
+
+  vector<string> const &getLibraryPaths()
+  {
+    return librarySearchPaths;
+  }
+
+  void appendLibraryPath(string const &dirname)
+  {
+    librarySearchPaths.push_back(dirname);
+  }
+
+  void prependLibraryPath(string const &dirname)
+  {
+    librarySearchPaths.insert(librarySearchPaths.begin(), dirname);
+  }
+
+  void setLibraryPaths(std::vector<std::string> const &paths)
+  {
+    librarySearchPaths = paths;
+  }
+
+  static void cleanLibraryMap()
+  {
+    map<string, xml_document *>::iterator it = libraryMap.begin();
+    while (it != libraryMap.end()) {
+      xml_document *temp = it->second;
+      libraryMap.erase(it);
+      delete temp;
+      it = libraryMap.begin();
+    }
+  }
+
+  void addLibraryNode(string const &name, xml_document *doc)
+  {
+    static bool sl_inited = false;
+    if (!sl_inited) {
+      addFinalizer(&cleanLibraryMap);
+      sl_inited = true;
+    }
+    assertTrue_2(doc, "addLibraryNode: Null document");
+    libraryMap[name] = doc;
+  }
+
+  xml_document *loadLibraryFile(string const &filename)
+  {
+    // Check current working directory first
+    xml_document *result = loadXmlFile(filename);
+    if (result)
+      return result;
+
+    // Find the first occurrence of the library in this path
+    vector<string>::const_iterator it = librarySearchPaths.begin();
+    while (!result && it != librarySearchPaths.end()) {
+      string candidateFile = *it + "/" + filename;
+      result = loadXmlFile(candidateFile);
+      if (result)
+        return result;
+      ++it;
+    }
+    return NULL;
+  }
+
+  xml_node loadLibraryNode(string const &name)
+  {
+    xml_document *doc = loadLibraryFile(name + ".plx");
+    if (!doc)
+      return xml_node();
+    xml_node theNode = doc->document_element().child(NODE_TAG);
+    xml_node nodeIdXml = theNode.child(NODEID_TAG);
+    checkParserExceptionWithLocation(nodeIdXml,
+                                     doc->document_element(),
+                                     "No " << NODEID_TAG << " element in library node, or not a PLEXIL plan");
+    char const *nodeId = nodeIdXml.child_value();
+    checkParserExceptionWithLocation(name == nodeId,
+                                     nodeIdXml,
+                                     "loadLibraryNode: Requested " << name
+                                     << " but file contains " << nodeId);
+    // *** TODO: handle global decls ***
+    addLibraryNode(name, doc);
+    return theNode;
+  }
+
+  xml_node getLibraryNode(string const &name, bool loadIfNotFound)
+  {
+    map<string, xml_document *>::iterator it = libraryMap.find(name);
+    if (it != libraryMap.end())
+      return it->second->document_element().child(NODE_TAG);
+    else if (loadIfNotFound)
+      return loadLibraryNode(name);
+    else
+      return xml_node();
+  }
+
+} // namespace PLEXIL
