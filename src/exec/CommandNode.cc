83e1afdffd67bcc1ec550230dfb8798f3850bd38
Chuck Fry <chuck.fry@nasa.gov>
2016-09-13 00:46:44 +0000

    Modify CommandHandleInterruptible operator to add support for COMMAND_INTERFACE_ERROR
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-4@4653 d2be67b0-814c-0410-949f-d215b6b29573

--- src/exec/CommandNode.cc
+++ src/exec/CommandNode.cc
@@ -64,12 +64,21 @@ namespace PLEXIL
     {
       uint16_t val;
       if (!arg->getValue(val)) // unknown
-        return false;
-      if (val == COMMAND_DENIED || val == COMMAND_FAILED)
+        return false; // result is unknown
+
+      switch (val) {
+        // Cases in which node is terminated early
+      case COMMAND_DENIED:          // Insufficient resources
+      case COMMAND_FAILED:          // Couldn't be sent/performed
+      case COMMAND_INTERFACE_ERROR: // Error in interface code
         result = true;
-      else
+        break;
+
+      default:
         result = false;
-      return true;
+        break;
+      }
+      return true; // result is known
     }
 
     DECLARE_OPERATOR_STATIC_INSTANCE(CommandHandleInterruptible, bool)
