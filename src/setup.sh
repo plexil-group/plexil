4ed880acc17727da2934699de439fa5e5bd9da15
Chuck Fry <chuck.fry@nasa.gov>
2017-07-21 01:50:33 +0000

    Deleted, replaced by scripts/plexil-setup.sh
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-4@4945 d2be67b0-814c-0410-949f-d215b6b29573

--- src/setup.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-export PLEXIL_HOME=/Users/chucko/src/plexil-4
-
-# Add to this as needed
-export DYLD_LIBRARY_PATH=$PLEXIL_HOME/src/third-party/pugixml/src/.libs:$PLEXIL_HOME/src/utils/.libs:$PLEXIL_HOME/src/expr/.libs:$PLEXIL_HOME/src/intfc/.libs:$PLEXIL_HOME/src/exec/.libs:$PLEXIL_HOME/src/interfaces/PlanDebugListener/.libs:$PLEXIL_HOME/src/interfaces/LuvListener/.libs:$PLEXIL_HOME/src/interfaces/Sockets/.libs:$PLEXIL_HOME/src/apps/TestExec/.libs:$PLEXIL_HOME/lib
-
-export PATH=$PATH:$PLEXIL_HOME/bin:$PLEXIL_HOME/scripts
-
