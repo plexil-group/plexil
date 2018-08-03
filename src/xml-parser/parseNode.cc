0d30bb920cefc02003b7cac785844b9a3ca44789
Chuck Fry <chuck.fry@nasa.gov>
2018-08-03 01:37:26 +0000

    Remove unused include file <cstdlib>
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-4@4982 d2be67b0-814c-0410-949f-d215b6b29573

--- src/xml-parser/parseNode.cc
+++ src/xml-parser/parseNode.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2017, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2018, Universities Space Research Association (USRA).
 *  All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -45,8 +45,7 @@
 
 #include "pugixml.hpp"
 
-#include <cstdlib> // for strtoul()
-#include <cstring>
+#include <cstring> // strcmp()
 
 using pugi::xml_attribute;
 using pugi::xml_node;
