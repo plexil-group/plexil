ffa0eaf2b6374c32a0e6ba5faef6b7fff250deb0
Chuck Fry <chuck.fry@nasa.gov>
2018-10-02 05:22:44 +0000

    Implement expression operators for node state predicates; not yet hooked in to parser
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-4@5027 d2be67b0-814c-0410-949f-d215b6b29573

---
+++ src/exec/NodeOperators.cc
@@ -0,0 +1,134 @@
+/* Copyright (c) 2006-2018, Universities Space Research Association (USRA).
+*  All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the Universities Space Research Association nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY USRA ``AS IS'' AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL USRA BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+* TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+* USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "NodeOperators.hh"
+
+#include "NodeImpl.hh"
+
+namespace PLEXIL
+{
+
+  NodeInactive::NodeInactive()
+    : NodeOperatorImpl<Boolean>("Inactive")
+  {
+  }
+
+  NodeInactive::~NodeInactive()
+  {
+  }
+
+  bool NodeInactive::checkArgCount(size_t count) const
+  {
+    return count == 1;
+  }
+
+  bool NodeInactive::operator()(Boolean &result, NodeImpl const *node) const
+  {
+    result = node->getState() == INACTIVE_STATE;
+    return true;
+  }
+
+  NodeWaiting::NodeWaiting()
+    : NodeOperatorImpl<Boolean>("Waiting")
+  {
+  }
+
+  NodeWaiting::~NodeWaiting()
+  {
+  }
+
+  bool NodeWaiting::checkArgCount(size_t count) const
+  {
+    return count == 1;
+  }
+
+  bool NodeWaiting::operator()(Boolean &result, NodeImpl const *node) const
+  {
+    result = node->getState() == WAITING_STATE;
+    return true;
+  }
+
+  NodeExecuting::NodeExecuting()
+    : NodeOperatorImpl<Boolean>("Executing")
+  {
+  }
+
+  NodeExecuting::~NodeExecuting()
+  {
+  }
+
+  bool NodeExecuting::checkArgCount(size_t count) const
+  {
+    return count == 1;
+  }
+
+  bool NodeExecuting::operator()(Boolean &result, NodeImpl const *node) const
+  {
+    result = node->getState() == EXECUTING_STATE;
+    return true;
+  }
+
+  NodeIterationEnded::NodeIterationEnded()
+    : NodeOperatorImpl<Boolean>("IterationEnded")
+  {
+  }
+
+  NodeIterationEnded::~NodeIterationEnded()
+  {
+  }
+
+  bool NodeIterationEnded::checkArgCount(size_t count) const
+  {
+    return count == 1;
+  }
+
+  bool NodeIterationEnded::operator()(Boolean &result, NodeImpl const *node) const
+  {
+    result = node->getState() == ITERATION_ENDED_STATE;
+    return true;
+  }
+
+  NodeFinished::NodeFinished()
+    : NodeOperatorImpl<Boolean>("Finished")
+  {
+  }
+
+  NodeFinished::~NodeFinished()
+  {
+  }
+
+  bool NodeFinished::checkArgCount(size_t count) const
+  {
+    return count == 1;
+  }
+
+  bool NodeFinished::operator()(Boolean &result, NodeImpl const *node) const
+  {
+    result = node->getState() == FINISHED_STATE;
+    return true;
+  }
+
+}
