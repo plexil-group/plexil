5cd027ebb6d81a0f9cd490442c38909dc94e42cd
Chuck Fry <chuck.fry@nasa.gov>
2020-02-11 03:17:11 +0000

    Simplify output format of mutex references
    git-svn-id: https://svn.code.sf.net/p/plexil/code/branches/plexil-x@5169 d2be67b0-814c-0410-949f-d215b6b29573

--- compilers/plexil/java/plexil/MutexName.java
+++ compilers/plexil/java/plexil/MutexName.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2017, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2020, Universities Space Research Association (USRA).
  *  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -62,9 +62,7 @@ public class MutexName extends PlexilName
     public IXMLElement asReference()
     {
         IXMLElement result = new XMLElement("Name");
-        IXMLElement valXML = new XMLElement("StringValue");
-        valXML.setContent(getName());
-        result.addChild(valXML);
+        result.setContent(getName());
         return result;
     }
 }
