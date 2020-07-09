24631b482ecd5927d2ecba36d4539446d0192239
akutsyy <albert@kutsyy.com>
2020-07-09 10:59:16 -0700

    Modified subscriber.cc to allow multiple subscriptions of one type

--- examples/sample-app/subscriber.cc
+++ examples/sample-app/subscriber.cc
@@ -1,12 +1,14 @@
 #include "subscriber.hh"
 #include "Debug.hh"
 
-using std::string;
 
+using std::string;
+using std::vector;
+using std::unordered_map;
 
 // Helper struct for hashing a vector (to use as the key in the unordered_map)
 struct VectorHasher {
-  int operator()(const std::vector<string> &V) const {
+  int operator()(const vector<string> &V) const {
         int hash = V.size();
 	std::hash<string> hasher;
 	for(string const &s : V) {
@@ -16,47 +18,47 @@ struct VectorHasher {
     }
 };
 
-//Maps arguments to functions
-static std::unordered_map<std::vector<string>,void* (*)(),VectorHasher> subscribers;
+//Maps arguments to a list of subscribed functions
+static unordered_map<vector<string>,vector<void* (*)()>,VectorHasher> subscribers;
 
 
 // Register a subscriber
-// Note that only one subscriber can be registered for each value/parameter combination
-// This was the original functionality and has been preserved, but could be changed easily
-// by switching from a single function pointer to a vector of function pointers as the map value type
 template<class ValueType, class ... ParamTypes>
 void setSubscriber (void (*receiver) (const string& state_name, ValueType val, ParamTypes ... args)){
 
   // Create a vector made of std::strings that detail the ValueType and then ParamTypes
-  std::vector<string> signature{string(typeid(ValueType).name()),string(typeid(ParamTypes).name())...};
+  vector<string> signature{string(typeid(ValueType).name()),string(typeid(ParamTypes).name())...};
  
   void* (*generic_pointer)() = reinterpret_cast<void*(*)()>(receiver);
-  subscribers.emplace(signature,generic_pointer);
-}
-/*
-// Backwards compatibility
-#define defSubscriber(signature) \
-void setSubscriber##signature (Subscribe##signature s) \
-{ \
-  setSubscriber(s);				\
+
+  // Check if vector of functions associated with this signature already exists in the map
+  auto got = subscribers.find (signature);
+  // If vector does not exist, make it
+  if ( got == subscribers.end() ){
+    vector<void* (*) ()> new_vector;
+    subscribers.emplace(signature,new_vector);
+  }
+  // Add the new subscribed function to the list of subscribed values for the signature
+  subscribers[signature].push_back(generic_pointer);
 }
 
-defSubscriber(Int)
-defSubscriber(Real)
-defSubscriber(String)
-defSubscriber(BoolString)
-defSubscriber(BoolIntInt)
-*/
+
 // Publish a state change to the appropriate subscriber
 
 template<class ValueType, class ... ParamTypes>
-void publish(const std::string& state_name, ValueType val, ParamTypes ... args){
+void publish(const string& state_name, ValueType val, ParamTypes ... args){
   // Create a vector made of std::strings that detail the ValueType and then ParamTypes
-  std::vector<string> signature (string(typeid(ValueType).name()),string(typeid(ParamTypes).name())...);
+  vector<string> signature (string(typeid(ValueType).name()),string(typeid(ParamTypes).name())...);
 
-  // Use that vector to retrieve the correct function and cast it to the correct type
-  void (*receiver) (const string& state_name, ValueType val, ParamTypes ... args);
-  receiver = reinterpret_cast<void (*) (const string& state_name, ValueType val, ParamTypes ... args)>(subscribers.at(signature));
-  // Calls the receiver function
-  receiver(state_name,val,args...);
+
+  // Retrieve vector of reception functions
+  vector<void* (*)()> receivers = subscribers.at(signature);
+  
+  // Use parameters to cast each function to the correct type and call it
+  for(auto &generic_receiver : receivers){
+    void (*receiver) (const string& state_name, ValueType val, ParamTypes ... args);
+    receiver = reinterpret_cast<void (*) (const string& state_name, ValueType val, ParamTypes ... args)>(generic_receiver);
+    // Calls the receiver function
+    receiver(state_name,val,args...);
+  }
 }
