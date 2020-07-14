964378b60e486aa51a222a4699f4288b6cbbd553
akutsyy <albert.kutsyy@nasa.gov>
2020-07-14 14:52:51 -0700

    cleaned up leftover files

---
+++ examples/checkpoint/StringAdapter.d
@@ -0,0 +1,33 @@
+StringAdapter.o StringAdapter.d : StringAdapter.cc StringAdapter.hh \
+ /home/albert/Desktop/NASA/plexil/include/Command.hh \
+ /home/albert/Desktop/NASA/plexil/include/CommandFunction.hh \
+ /home/albert/Desktop/NASA/plexil/include/Expression.hh \
+ /home/albert/Desktop/NASA/plexil/include/ValueType.hh \
+ /home/albert/Desktop/NASA/plexil/include/ParserException.hh \
+ /home/albert/Desktop/NASA/plexil/include/Error.hh \
+ /home/albert/Desktop/NASA/plexil/include/ArrayFwd.hh \
+ /home/albert/Desktop/NASA/plexil/include/plexil-stdint.h \
+ /home/albert/Desktop/NASA/plexil/include/plexil-config.h \
+ /home/albert/Desktop/NASA/plexil/include/Listenable.hh \
+ /home/albert/Desktop/NASA/plexil/include/ExpressionListener.hh \
+ /home/albert/Desktop/NASA/plexil/include/Propagator.hh \
+ /home/albert/Desktop/NASA/plexil/include/Notifier.hh \
+ /home/albert/Desktop/NASA/plexil/include/CommandHandleVariable.hh \
+ /home/albert/Desktop/NASA/plexil/include/CommandHandle.hh \
+ /home/albert/Desktop/NASA/plexil/include/GetValueImpl.hh \
+ /home/albert/Desktop/NASA/plexil/include/State.hh \
+ /home/albert/Desktop/NASA/plexil/include/Value.hh \
+ /home/albert/Desktop/NASA/plexil/include/Array.hh \
+ /home/albert/Desktop/NASA/plexil/include/SimpleBooleanVariable.hh \
+ /home/albert/Desktop/NASA/plexil/include/Value.hh \
+ /home/albert/Desktop/NASA/plexil/include/InterfaceAdapter.hh \
+ /home/albert/Desktop/NASA/plexil/include/pugixml.hpp \
+ /home/albert/Desktop/NASA/plexil/include/pugiconfig.hpp \
+ /home/albert/Desktop/NASA/plexil/include/AdapterConfiguration.hh \
+ /home/albert/Desktop/NASA/plexil/include/InterfaceAdapter.hh \
+ /home/albert/Desktop/NASA/plexil/include/AdapterFactory.hh \
+ /home/albert/Desktop/NASA/plexil/include/AdapterExecInterface.hh \
+ /home/albert/Desktop/NASA/plexil/include/Debug.hh \
+ /home/albert/Desktop/NASA/plexil/include/DebugMessage.hh \
+ /home/albert/Desktop/NASA/plexil/include/Expression.hh \
+ /home/albert/Desktop/NASA/plexil/include/StateCacheEntry.hh
---
+++ examples/checkpoint/Subscriber.d
@@ -0,0 +1,25 @@
+Subscriber.o Subscriber.d : Subscriber.cc Subscriber.hh \
+ /home/albert/Desktop/NASA/plexil/include/Value.hh \
+ /home/albert/Desktop/NASA/plexil/include/ValueType.hh \
+ /home/albert/Desktop/NASA/plexil/include/ParserException.hh \
+ /home/albert/Desktop/NASA/plexil/include/Error.hh \
+ /home/albert/Desktop/NASA/plexil/include/ArrayFwd.hh \
+ /home/albert/Desktop/NASA/plexil/include/plexil-stdint.h \
+ /home/albert/Desktop/NASA/plexil/include/plexil-config.h \
+ /home/albert/Desktop/NASA/plexil/include/Array.hh CheckpointAdapter.hh \
+ /home/albert/Desktop/NASA/plexil/include/Command.hh \
+ /home/albert/Desktop/NASA/plexil/include/CommandFunction.hh \
+ /home/albert/Desktop/NASA/plexil/include/Expression.hh \
+ /home/albert/Desktop/NASA/plexil/include/Listenable.hh \
+ /home/albert/Desktop/NASA/plexil/include/ExpressionListener.hh \
+ /home/albert/Desktop/NASA/plexil/include/Propagator.hh \
+ /home/albert/Desktop/NASA/plexil/include/Notifier.hh \
+ /home/albert/Desktop/NASA/plexil/include/CommandHandleVariable.hh \
+ /home/albert/Desktop/NASA/plexil/include/CommandHandle.hh \
+ /home/albert/Desktop/NASA/plexil/include/GetValueImpl.hh \
+ /home/albert/Desktop/NASA/plexil/include/State.hh \
+ /home/albert/Desktop/NASA/plexil/include/Value.hh \
+ /home/albert/Desktop/NASA/plexil/include/SimpleBooleanVariable.hh \
+ /home/albert/Desktop/NASA/plexil/include/InterfaceAdapter.hh \
+ /home/albert/Desktop/NASA/plexil/include/pugixml.hpp \
+ /home/albert/Desktop/NASA/plexil/include/pugiconfig.hpp
