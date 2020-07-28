141b259098ffc2b27b33cfa92c20b3442f8c32fe
akutsyy <albert.kutsyy@nasa.gov>
2020-07-28 11:57:37 -0700

    Made constructor/destructor/copy choices explicit,changed naming conventions,fixed warning in StringOperations

--- examples/checkpoint/CheckpointSystem.hh
+++ examples/checkpoint/CheckpointSystem.hh
@@ -120,7 +120,7 @@ private:
   SaveManager* const m_manager;
   
   // Data
-  std::vector<boot_data>  m_data_vector;
+  std::vector<BootData>  m_data_vector;
   int m_num_total_boots;
   
   // Data
