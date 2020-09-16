8dcd9a6bfb8b2338d98d95f61dcd0069710bde11
Chuck Fry <Chuck.Fry@NASA.GOV>
2020-09-15 18:29:24 -0700

    Remove redundant #include statement for string

--- src/xml-parser/planLibrary.hh
+++ src/xml-parser/planLibrary.hh
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2018, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2020, Universities Space Research Association (USRA).
 *  All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -27,10 +27,9 @@
 #ifndef PLEXIL_PLAN_LIBRARY_HH
 #define PLEXIL_PLAN_LIBRARY_HH
 
-#include <string>
-#include <vector>
-
 #include "ParserException.hh"
+
+#include <vector>
 
 namespace pugi
 {
