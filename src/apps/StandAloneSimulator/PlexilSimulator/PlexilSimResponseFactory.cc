e049f13c7e2b48ee28a3eef979280f830ee9f583
Chuck Fry <Chuck.Fry@NASA.GOV>
2020-09-16 17:10:02 -0700

    Fix include-what-you-use related error

--- src/apps/StandAloneSimulator/PlexilSimulator/PlexilSimResponseFactory.cc
+++ src/apps/StandAloneSimulator/PlexilSimulator/PlexilSimResponseFactory.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2008, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2020, Universities Space Research Association (USRA).
 *  All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -29,6 +29,8 @@
 #include "simdefs.hh"
 
 #include "Debug.hh"
+
+#include <sstream>
 
 
 PlexilSimResponseFactory::PlexilSimResponseFactory()
