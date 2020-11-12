32f33818ef5a4e82ae6485df325c24d83aea40c9
Chuck Fry <chucko@chucko.com>
2020-11-11 22:11:43 -0800

    Fix some missing Standard C++ Library includes

--- src/exec/NodeTimepointValue.cc
+++ src/exec/NodeTimepointValue.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2018, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2020, Universities Space Research Association (USRA).
  *  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,6 +29,7 @@
 #include "NodeConnector.hh"
 
 #include <iomanip> // for std::setprecision()
+#include <ostream>
 
 namespace PLEXIL
 {
