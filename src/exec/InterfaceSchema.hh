a1ab91692b3f6479e955719d02cb8a1524f1b9dc
Chuck Fry <Chuck.Fry@NASA.GOV>
2020-12-04 22:44:22 -0800

    InterfaceSchema.hh: Revise interface configuration schema

--- src/exec/InterfaceSchema.hh
+++ src/exec/InterfaceSchema.hh
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2016, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2020, Universities Space Research Association (USRA).
 *  All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -33,27 +33,28 @@ namespace PLEXIL
   /**
    * @brief This class provides utilities related to the generic interface XML schema.
    */
-
-  class InterfaceSchema
+  struct InterfaceSchema
   {
-  public:
-
     //
     // XML tags
     //
 
     static constexpr char const *ADAPTER_TAG = "Adapter";
+    static constexpr char const *COMMAND_HANDLER_TAG = "CommandHandler";
     static constexpr char const *COMMAND_NAMES_TAG = "CommandNames";
     static constexpr char const *DEFAULT_ADAPTER_TAG = "DefaultAdapter";
     static constexpr char const *DEFAULT_COMMAND_ADAPTER_TAG = "DefaultCommandAdapter";
     static constexpr char const *DEFAULT_LOOKUP_ADAPTER_TAG = "DefaultLookupAdapter";
     static constexpr char const *FILTER_TAG = "Filter";
     static constexpr char const *INTERFACES_TAG = "Interfaces";
+    static constexpr char const *INTERFACE_LIBRARY_TAG = "InterfaceLibrary";
     static constexpr char const *LIBRARY_NODE_PATH_TAG = "LibraryNodePath";
     static constexpr char const *LISTENER_TAG = "Listener";
+    static constexpr char const *LOOKUP_HANDLER_TAG = "LookupHandler";
     static constexpr char const *LOOKUP_NAMES_TAG = "LookupNames";
     static constexpr char const *PLAN_PATH_TAG = "PlanPath";
     static constexpr char const *PLANNER_UPDATE_TAG = "PlannerUpdate";
+    static constexpr char const *PLANNER_UPDATE_HANDLER_TAG = "PlannerUpdateHandler";
     static constexpr char const *IP_ADDRESS_TAG = "IpAddress";
     static constexpr char const *PORT_NUMBER_TAG = "PortNumber";
 
@@ -62,17 +63,18 @@ namespace PLEXIL
     //
 
     static constexpr char const *ADAPTER_TYPE_ATTR = "AdapterType";
-    static constexpr char const *LIB_PATH_ATTR = "LibPath";
+    static constexpr char const *DEFAULT_HANDLER_ATTR = "DefaultHandler";
     static constexpr char const *FILTER_TYPE_ATTR = "FilterType";
+    static constexpr char const *HANDLER_TYPE_ATTR = "HandlerType";
+    static constexpr char const *LIB_PATH_ATTR = "LibPath";
     static constexpr char const *LISTENER_TYPE_ATTR = "ListenerType";
-    static constexpr char const *TELEMETRY_ONLY_ATTR = "TelemetryOnly";
+    static constexpr char const *NAME_ATTR = "Name";
 
     /**
-     * Extract comma separated arguments from a character string.
+     * @brief Extract comma separated arguments from a character string.
      * @return pointer to vector of strings
      * @note Caller is responsible for disposing of the vector.
      */
-
     static std::vector<std::string> * parseCommaSeparatedArgs(const char * argString);
 
   };
