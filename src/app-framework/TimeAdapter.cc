da92bbead8a58a753b074ab0c14b45ee77c6907b
Chuck Fry <Chuck.Fry@NASA.GOV>
2021-01-05 01:06:03 -0800

    TimeAdapter.cc: TimebaseFactory function makeTimebase() no longer a class member

--- src/app-framework/TimeAdapter.cc
+++ src/app-framework/TimeAdapter.cc
@@ -128,8 +128,7 @@ namespace PLEXIL
     {
       // Construct Timebase
       pugi::xml_node const tb_xml = getXml().child(InterfaceSchema::TIMEBASE_TAG);
-      m_timebase.reset(TimebaseFactory::makeTimebase(tb_xml, &timeout,
-                                                     (void *) this));
+      m_timebase.reset(makeTimebase(tb_xml, &timeout, (void *) this));
       config->registerLookupHandler(new TimeLookupHandler(m_timebase.get()),
                                     "time");
       return true;
