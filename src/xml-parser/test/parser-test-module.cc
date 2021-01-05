3ec1716110add841a486fb533997d7d676ecd54a
Chuck Fry <Chuck.Fry@NASA.GOV>
2021-01-04 22:24:28 -0800

    parser-test-module.cc: Add #include "PlanError.hh"

--- src/xml-parser/test/parser-test-module.cc
+++ src/xml-parser/test/parser-test-module.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2020, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2021, Universities Space Research Association (USRA).
 *  All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -26,6 +26,7 @@
 
 #include "DebugMessage.hh"
 #include "lifecycle-utils.h"
+#include "PlanError.hh"
 #include "SymbolTable.hh"
 #include "TestSupport.hh"
 
