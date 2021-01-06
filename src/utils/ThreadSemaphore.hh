59ef578028a42d086c892e1ce129f149a7b12d36
Chuck Fry <Chuck.Fry@NASA.GOV>
2021-01-05 21:46:00 -0800

    ThreadSemaphore: Reimplement as handle/implementation

--- src/utils/ThreadSemaphore.hh
+++ src/utils/ThreadSemaphore.hh
@@ -27,103 +27,52 @@
 #ifndef PLEXIL_THREAD_SEMAPHORE_HH
 #define PLEXIL_THREAD_SEMAPHORE_HH
 
-#include "plexil-config.h"
-
-//
-// Does this platform support POSIX thread semaphores?
-//
-
-#ifdef HAVE_UNISTD_H
-#include <unistd.h>
-#endif
-
-#if !defined(_POSIX_SEMAPHORES) || _POSIX_SEMAPHORES == -1
-// OS doesn't advertise it provides POSIX semaphores, or they're broken somehow
-#ifdef __MACH__
-#define PLEXIL_USE_MACH_SEMAPHORES // e.g. Mac OS X
-#elif defined(__VXWORKS__)
-#define PLEXIL_USE_POSIX_SEMAPHORES
-#endif // __VXWORKS __
-
-#else 
-// OS provides POSIX semaphores
-#define PLEXIL_USE_POSIX_SEMAPHORES
-#endif // _POSIX_SEMAPHORES == -1
-
-//
-// Include the native semaphore headers
-//
-
-#ifdef PLEXIL_USE_POSIX_SEMAPHORES
-#include <semaphore.h>
-#if defined(HAVE_CERRNO)
-#include <cerrno>
-#elif defined(HAVE_ERRNO_H)
-#include <errno.h>
-#endif
-
-//* @brief Error status showing that the wait() call was interrupted.
-#define PLEXIL_SEMAPHORE_STATUS_INTERRUPTED EINTR
-#endif // PLEXIL_USE_POSIX_SEMAPHORES
-
-#ifdef PLEXIL_USE_MACH_SEMAPHORES
-#include <mach/kern_return.h>  // for KERN_ABORTED
-#include <mach/mach_types.h>   // for semaphore_t, task_t
-
-//* @brief Error status showing that the wait() call was interrupted.
-#define PLEXIL_SEMAPHORE_STATUS_INTERRUPTED KERN_ABORTED
-#endif // PLEXIL_USE_MACH_SEMAPHORES
+#include <memory>
 
 namespace PLEXIL 
 {
+
+  // Forward reference
+  class ThreadSemaphoreImpl;
+
   /**
      @class ThreadSemaphore
      @brief Provides a cross-platform API to binary thread semaphores.
      @author Chuck Fry
-     @date 11 July 2008
    */
 
   class ThreadSemaphore
   {
   public:
-    /**
-       @brief Constructor.
-    */
+    //! Default constructor.
     ThreadSemaphore();
 
-    /**
-       @brief Destructor.
-    */
-    ~ThreadSemaphore();
+    //! Destructor.
+    ~ThreadSemaphore() = default;
 
-    /**
-       @brief Causes the thread to block until post() is called on the semaphore.
-       @return 0 if successful, error number otherwise.
-       @note Error number is platform dependent.
-	   @note A status of PLEXIL_SEMAPHORE_STATUS_INTERRUPTED means the system call was interrupted,
-             e.g. by a signal or pthread_cancel(). This may not be an error depending on the situation.
-			 The caller should always check the return value and deal with it appropriately!
-    */
+    //! Causes the caller's thread to block until post() is called on the semaphore.
+    //! @return 0 if successful, error number otherwise.
+    //! @note Error number is platform dependent.
     int wait();
 
-    /**
-       @brief Causes the semaphore to unblock a thread currently waiting on it.
-       @return 0 if successful, error number otherwise.
-       @note Error number is platform dependent.
-    */
+    //! Causes the semaphore to unblock a thread currently waiting on it.
+    //! @return 0 if successful, error number otherwise.
+    //! @note Error number is platform dependent.
     int post();
 
   private:
 
-#ifdef PLEXIL_USE_POSIX_SEMAPHORES
-    sem_t m_posix_sem;
-#endif // PLEXIL_USE_POSIX_SEMAPHORES
+    //! Pointer to the implementation object.
+    std::unique_ptr<ThreadSemaphoreImpl> m_impl;
+  };
 
-#ifdef PLEXIL_USE_MACH_SEMAPHORES
-    semaphore_t m_mach_sem;
-    task_t m_mach_owning_task;
-#endif // PLEXIL_USE_MACH_SEMAPHORES
-
+  // Pure virtual base class for the implementation
+  class ThreadSemaphoreImpl
+  {
+  public:
+    virtual int wait() = 0;
+    virtual int post() = 0;
+    virtual ~ThreadSemaphoreImpl() = default;
   };
 
 } // namespace PLEXIL
