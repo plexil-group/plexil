5d3b495c7bda241067f6eac0ce9d9264d5db7c00
Chuck Fry <Chuck.Fry@NASA.GOV>
2021-01-27 13:50:40 -0800

    regression.pl: Cleaned up some, needs more work

--- test/TestExec-regression-test/regression.pl
+++ test/TestExec-regression-test/regression.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl
 
-# Copyright (c) 2006-2014, Universities Space Research Association (USRA).
+# Copyright (c) 2006-2021, Universities Space Research Association (USRA).
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -25,92 +25,103 @@
 # TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+use strict;
+use warnings;
 
-$fileout = 'RegressionResults';
-$file1 = @ARGV[0];
-$file2 = @ARGV[1];
+my $fileout = 'RegressionResults';
+my $testfile = $ARGV[0];
+my $validfile = $ARGV[1];
 
-open(DIF, ">>$fileout");	# Open for output
-$testName = $file1;
+my $encoding = ":encoding(ISO-8859-1)"; # ??
+
+my $exitstatus = 0;
+
+open(DIF, ">> $encoding", $fileout); # Open for append
+
+my $testName = $testfile;
 $testName =~ s/output\/RUN_(.*)_(.*)\.out/$1 + $2/;
 
+sub stripline {
+    my $line = shift(@_);
+    $line =~ s/(0x[0-9a-fA-F]{2,16})//g;
+    return $line;
+}
 
 # this is for comparing .out with .valid
 # only if a .valid file is passed in
-if ($file2){     
+if ($validfile) {
+    open(TEST, "< $encoding", $testfile);
+    open(VALID, "< $encoding", $validfile);
+    my @lines1 = <TEST>;
+    my @lines2 = <VALID>;
+    my $n = 0;
+    my $m = 0;
+    # FIXME: case where valid file has more lines than test
+    # FIXME: case where test file has more lines than valid
+    foreach my $validline (@lines2) {
+        $validline = stripline($validline);
+        my $testline = stripline($lines1[$n]);
+        if ($testline ne $validline) {
+            if ($m == 0) {
+                print DIF "\n";
+                print DIF "**** TEST FAILED: ", $testName; 
+                print STDERR "\nTEST FAILED:  ", $testName;
+                print STDERR "***\n  ------ .out file differs from .valid file\n";
+            }
+            $m++;
+            print DIF "\nLine: ", $n;
+            print DIF "\n";
+            print DIF ".out:   ", $testline; 
+            print DIF ".valid: ", $validline;  
+            print DIF "\n";
+        }  
+        $n++;
+    }
+    if ($n == 0) {
+        print DIF "\n";
+        print DIF "\n**** TEST FAILED: ", $testName; 
+        print DIF "***\n  ------ No .output file was provided\n";
+        print STDERR "\nTEST FAILED:  ", $testName, "\n";
+        print STDERR "  ------ No .output file was provided\n";
+    } elsif ($m != 0) {
+        $exitstatus = 1;
+    }
+    close(VALID);
+    close(TEST);
+} else {
+    # Check for success of root node
+    # for files where this enough to show success
+    open(TEST, "< $encoding", $testfile);
 
-open (F1, $file1);
-open (F2, $file2);
-open(FE, STDERR);
-@lines1 = <F1>;
-@lines2 = <F2>;
-$n = 0;
-$m = 0;
-foreach $temp (@lines2){
-  $line1 = @lines1[$n];
-  $line1 =~ s/(0x[0-9a-fA-F]{2,16})//g;
-  $temp =~ s/(0x[0-9a-fA-F]{2,16})//g;
- if ($line1 ne $temp){   
-      if ($m == 0){
-          print DIF "\n";
-          print DIF "**** TEST FAILED: ", $testName; 
-          print "\nTEST FAILED:  ", $testName;
-          print "***\n  ------ .out file differs from .valid file\n";
-      }
-      $m++;
-      print DIF "\nLine: ", $n;
-      print DIF "\n";
-      print DIF ".out:   ", $line1; 
-      print DIF ".valid: ", $temp;  
-      print DIF "\n";
- 
-  }  
-$n++  
-}
-if ($n == 0){
-          print DIF "\n";
-          print DIF "\n**** TEST FAILED: ", $testName; 
-          print DIF "***\n  ------ No .output file was provided\n";
-          print "\nTEST FAILED:  ", $testName, "\n";
-          print "  ------ No .output file was provided\n";
-}elsif ($m == 0){
-   print DIF "\nTEST PASSED: ", $testName, "\n"; 
-}
-close(F2);
-
-}else{
-# Check for success of root node
-# for files where this enough to show success
-    open (F1, $file1);
-    
-    @lines1 = <F1>;
-    $n = 0;
-    $k = 0;
-    foreach $line1 (@lines1){
-        if ($line1 =~ m/Added plan:/){
-            if (@lines1[$n+1] =~ m/^(.*)\{$/){
-                $rootNode = $1;
-                $count = @lines1;
-                for ($i = $count-1; $i > 0; $i--){
-                    if(@lines1[$i] =~ m/PlexilExec:printPlan/){
-                         $k = 1;
-                        if (@lines1[$i+1] =~ m/^$rootNode\{$/){
-                            if (@lines1[$i+3] =~ m/^ Outcome: ([A-Z]*)$/){
-							  $outcome = $1;
-                                if ($outcome eq "SUCCESS"){
-                                    print DIF "\nTEST PASSED: ", $testName, "\n";
+    my @lines1 = <TEST>;
+    my $n = 0;
+    my $k = 0;
+    foreach my $line1 (@lines1) {
+        if ($line1 =~ m/Added plan:/ ){
+            if ($lines1[$n+1] =~ m/^(.*)\{$/) {
+                my $rootNode = $1;
+                my $count = @lines1;
+                for (my $i = $count-1; $i > 0; $i--) {
+                    if($lines1[$i] =~ m/PlexilExec:printPlan/) {
+                        $k = 1;
+                        if ($lines1[$i+1] =~ m/^$rootNode\{$/) {
+                            if ($lines1[$i+3] =~ m/^ Outcome: ([A-Z]*)$/) {
+                                my $outcome = $1;
+                                if ($outcome eq "SUCCESS") {
                                     $i = 0;
-                                }else{
+                                } else {
                                     print DIF "\n";
                                     print DIF "\n*** TEST FAILED:  ", $testName, " ***\n  ------- root node outcome = ", $outcome, "\n";
-                                    print "\nTEST FAILED:  ", $testName, "\n  ------- root node outcome = ", $outcome, "\n";
+                                    print STDERR "\nTEST FAILED:  ", $testName, "\n  ------- root node outcome = ", $outcome, "\n";
                                     $i = 0;
+                                    $exitstatus = 1;
                                 }
-                            }else{
+                            } else {
                                 print DIF "\n";
                                 print DIF "*** TEST FAILED:  ", $testName, " ***\n  ------- root node outcome != SUCCESS\n";
-                                print "\nTEST FAILED:  ", $testName, "\n  ------- root node outcome != SUCCESS\n";
+                                print STDERR "\nTEST FAILED:  ", $testName, "\n  ------- root node outcome != SUCCESS\n";
                                 $i = 0;
+                                $exitstatus = 1;
                             }
                         }
                     }
@@ -119,33 +130,33 @@ close(F2);
         }
         $n++;
     }
-    if ($k == 0){
-      print DIF "\n";
-      print DIF "*** TEST FAILED:  ", $testName, " ***\n  ------- ERROR in test execution\n";
-      print "\nTEST FAILED:  ", $testName, "\n  ------- ERROR in test execution\n";
-   }
- }
-
-#this doesn't work (tempName) with libraries yet
-$fileErrs = 'tempRegressionResults';
-open (F3, $fileErrs);
-my $i = 0;
-@lines3 = <F3>;
-foreach $line3 (@lines3){
-    $i++;
-   if ($line3 =~ s/^RUN_exec-test-runner_g_rt\.-s\.(.*?)\.xml\.-p\.(.*?)\.xml.*/$2 + $1/)
-   {
-      chomp($tempName = $line3);
-      if ($tempName eq $testName){    
-         $line3 = @lines3[$i++];
-         while ($line3 && !($line3 =~ m/^RUN_exec-test-runner_g_rt\.-s\.(.*?)\.xml\.-p\.(.*?)\.xml.*/)){
-            print DIF $line3;
-               $line3 = @lines3[$i++];
-          }
-       }
-  }
+    if ($k == 0) {
+        print DIF "\n";
+        print DIF "*** TEST FAILED:  ", $testName, " ***\n  ------- ERROR in test execution\n";
+        print STDERR "\nTEST FAILED:  ", $testName, "\n  ------- ERROR in test execution\n";
+    }
+    close(TEST);
 }
 
-close(F1);
-close(F2);
+# this doesn't work (tempName) with libraries yet
+my $fileErrs = 'tempRegressionResults';
+open(F3, "< $encoding", $fileErrs);
+my $i = 0;
+my @lines3 = <F3>;
+foreach my $line3 (@lines3) {
+    $i++;
+    if ($line3 =~ s/^RUN_exec-test-runner_g_rt\.-s\.(.*?)\.xml\.-p\.(.*?)\.xml.*/$2 + $1/) {
+        chomp(my $tempName = $line3);
+        if ($tempName eq $testName) {
+            my $line3 = $lines3[$i++];
+            while ($line3 && !($line3 =~ m/^RUN_exec-test-runner_g_rt\.-s\.(.*?)\.xml\.-p\.(.*?)\.xml.*/)) {
+                print DIF $line3;
+                $line3 = $lines3[$i++];
+            }
+        }
+    }
+}
+close(F3);
 close(DIF);
+
+exit $exitstatus;
