27d6deff670004a060f432f2a615a477135d6984
Chuck Fry <Chuck.Fry@NASA.GOV>
2021-02-02 10:23:28 -0800

    test-sas.sh: Default PLEXIL_HOME if not set; exit with message and error if incorrectly set.

--- src/apps/StandAloneSimulator/test/test-sas.sh
+++ src/apps/StandAloneSimulator/test/test-sas.sh
@@ -1,17 +1,29 @@
-#! /bin/sh
+#! /bin/sh -e
 # Run the SAS test
 
-export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/bin
-
-set -e
+guess_plexil_home()
+{
+    # This test script is expected to be in $PLEXIL_HOME/src/apps/StandAloneSimulator/test
+    TEST_DIR="$( cd "$(dirname "$(command -v "$0")")" && pwd -P )"
+    cd "$TEST_DIR/../../../.." && pwd -P
+}
 
 if [ -z "$PLEXIL_HOME" ]
 then
-    echo "Error: Unable to set up Plexil environment."
-    echo "Please set environment variable PLEXIL_HOME"
-    echo "to the full pathname of your PLEXIL installation directory."
-    return 1
+    PLEXIL_HOME="$(guess_plexil_home)"
+    export PLEXIL_HOME
+    echo "Defaulting PLEXIL_HOME to $PLEXIL_HOME"
 fi
+
+if [ ! -x "$PLEXIL_HOME/scripts/plexil-setup.sh" ]
+then
+    echo 'Error: Environment variable PLEXIL_HOME is set incorrectly.' >&2
+    echo 'Please set it to the full pathname of the PLEXIL source directory' >&2
+    echo " (presumably $(guess_plexil_home))" >&2
+    exit 1
+fi
+
+export PLEXIL_HOME
 
 . "$PLEXIL_HOME"/scripts/plexil-setup.sh
 
