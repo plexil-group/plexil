c929836c9a6261d5bc6e33a36dded1e40e9d9c1d
Chuck Fry <chuck.fry@nasa.gov>
2021-02-12 23:44:41 -0800

    udp-tests.cc: Partition existing tests; add test of parse_hostname(), UdpEventLoop

--- src/interfaces/UdpAdapter/test/udp-tests.cc
+++ src/interfaces/UdpAdapter/test/udp-tests.cc
@@ -27,10 +27,10 @@
 #include "plexil-inttypes.h"  // fixed width integer formats
 
 #include "udp-utils.hh"
-
-#include "ThreadSpawn.hh" // *** DELETE ME ***
+#include "UdpEventLoop.hh"
 
 #include <iostream>
+#include <thread>
 
 #ifdef HAVE_NETINET_IN_H
 #include <netinet/in.h> // IPPROTO_UDP
@@ -47,28 +47,58 @@
 
 using namespace PLEXIL;
 
-static int test_input_wait_thread(udp_thread_params *params)
+static bool testHostnameParsing()
 {
-  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-  if (sock < 1) {
-    char buf[50];
-    sprintf(buf, "test_input_wait_thread: socket() returned -1");
-    perror(buf);
-    return sock;
+  std::cout << "Testing hostname parsing" << std::endl;
+  bool result = true;
+
+  // nnn.nnn.nnn.nnn format
+  char text_127_0_0_1[] = "127.0.0.1";
+  in_addr_t addr_127_0_0_1 = parse_hostname(text_127_0_0_1);
+  if (!addr_127_0_0_1) {
+    std::cerr << "Parsing of \"" << text_127_0_0_1 << "\" failed" << std::endl;
+    result = false;
   }
-  params->sock = sock;
-  return wait_for_input(params->local_port, params->buffer, params->size, sock, params->debug);
+  else if (addr_127_0_0_1 != (in_addr_t) 0x0100007F) {
+    std::cerr << "Parsing of \"" << text_127_0_0_1 << "\" returned incorrect result" << std::endl;
+    result = false;
+  }
+  else {
+    std::cout << "Parsing of \"" << text_127_0_0_1 << "\" succeeded" << std::endl;
+  }
+
+  // localhost
+  char text_localhost[] = "localhost";
+  in_addr_t addr_localhost = parse_hostname(text_localhost);
+  if (!addr_localhost) {
+    std::cerr << "Parsing of \"" << text_localhost << "\" failed" << std::endl;
+    result = false;
+  }
+  else if (addr_localhost != addr_127_0_0_1) {
+    std::cerr << "Result for \"" << text_localhost << "\" doesn't match result for \""
+              << text_127_0_0_1 << std::endl;
+    result = false;
+  }
+  else {
+        std::cout << "Parsing of \"" << text_localhost << "\" succeeded" << std::endl;
+  }
+
+  // more later
+
+  std::cout << std::endl;
+  return result;
 }
 
-int main()
+static bool testEncodeDecode()
 {
-  unsigned char* bytes1 = new unsigned char[32];
-  unsigned char* bytes2 = new unsigned char[32];
+  unsigned char bytes1[32];
+  unsigned char bytes2[32];
 
   bytes1[0] = 0x91;                   // 145
   bytes1[1] = 0x16;                   //  22
   bytes1[2] = 0x4D;                   //  77
   bytes1[3] = 0xE4;                   // 228
+  bytes1[4] = 0;                      // null char for string test
 
   printf("\nBasic encoding, decoding, and shifting\n");
 
@@ -81,6 +111,7 @@ int main()
   print_buffer(bytes2, 8);
 
   // read the network bytes and extract the 32 bit integer
+  printf("\Encode and decode int32_t\n");
   int32_t temp = decode_int32_t(bytes1, 0);
   std::cout << "-1860809244 == " << temp << std::endl << std::endl;
   // convert the 32 bit integer back to a byte stream
@@ -91,6 +122,7 @@ int main()
   print_buffer(bytes2, 8);
 
   // convert a subset of the network bytes
+  printf("\nEncode and decode short_int\n");
   temp = decode_short_int(bytes1, 1);
   std::cout << "5709 == " << temp << std::endl;
 
@@ -137,6 +169,27 @@ int main()
   printf("\npif=decode_float(bytes2, 0)\n");
   printf("pif=%f, pii=%d\n", pif, pii);
 
+  return true;
+}
+
+static int test_input_wait_thread(udp_thread_params *params)
+{
+  int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (sock < 1) {
+    char buf[50];
+    sprintf(buf, "test_input_wait_thread: socket() returned -1");
+    perror(buf);
+    return sock;
+  }
+  params->sock = sock;
+  return wait_for_input(params->local_port, params->buffer, params->size, sock, params->debug);
+}
+
+static bool testSendReceive()
+{
+  unsigned char bytes1[32];
+  unsigned char bytes2[32];
+
   printf("\nSend and receive some UDP buffers\n\n");
 
   int32_t local_port = 9876;
@@ -148,8 +201,7 @@ int main()
   // Socket for the thread waiting for input
   // Parameters for the thread waiting for input
   udp_thread_params the_params = { bytes2, 32, remote_port, 0, true };
-  pthread_t thread_handle;
-  threadSpawn((THREAD_FUNC_PTR) test_input_wait_thread, &the_params, thread_handle);
+  std::thread thread_handle(test_input_wait_thread, &the_params);
 
   // Wait for listener to establish socket
   do {
@@ -168,18 +220,77 @@ int main()
   }
 
   // Wait for wait_for_input to return
-  int myErrno = pthread_join(thread_handle, nullptr);
-  if (myErrno != 0)
-    printf("pthread_join(thread_handle) returned %d\n", myErrno);
+  thread_handle.join();
 
   printf("\n");
   print_buffer(bytes1, 32, true);
   print_buffer(bytes2, 32, true);
 
   printf("\nDone.\n\n");
+  return true;
+}
 
-  delete[] bytes1;
-  delete[] bytes2;
+// Event loop listener function
+static void eventListener(in_port_t port,
+                          const void *buffer,
+                          size_t length,
+                          const struct sockaddr *address,
+                          socklen_t address_len)
+{
+  std::cout << "Event listener: received " << length << " bytes on port " << port << std::endl;
+}
+
+static bool testEventLoop()
+{
+  in_port_t local_port = 9876;
+  char remote_host[] = "localhost";
+  in_port_t remote_port = 8031;
+
+
+  std::cout << "Test UdpEventLoop" << std::endl;
+  std::unique_ptr<UdpEventLoop> loop = makeUdpEventLoop();
+  std::cout << "Test start" << std::endl;
+  if (loop->start()) {
+    std:: cout << "Loop start succeeded" << std::endl;
+  }
+  else {
+    std:: cout << "Loop start failed. Ending test." << std::endl;
+    return false;
+  }
+  std::cout << "Test openListener" << std::endl;
+  if (loop->openListener(remote_port, 512, eventListener)) {
+    std::cout << "openListener succeeded" << std::endl;
+  }
+  else {
+    std::cout << "openListener failed. Ending test." << std::endl;
+    return false;
+  }
+
+  // Test listener
+  // TODO
+
+  std::cout << "Test closeListener" << std::endl;
+  loop->closeListener(remote_port);
+
+  std::cout << "Test stop" << std::endl;
+  loop->stop();
+
+  return true;
+}
+
+int main()
+{
+  testEncodeDecode();
+
+  if (!testHostnameParsing()) {
+    std::cerr << "Hostname parsing failed. Aborting test." << std::endl;
+    return 1;
+  }
+
+  testSendReceive();
+
+  testEventLoop();
+
   return 0;
 }
 
