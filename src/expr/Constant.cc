3e7ce91d70a919ed457d4736dffdd99f121f76c4
Chuck Fry <Chuck.Fry@NASA.GOV>
2021-05-06 12:28:02 -0700

    Constant template class: declare destructor as default

--- src/expr/Constant.cc
+++ src/expr/Constant.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2018, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2021, Universities Space Research Association (USRA).
 *  All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -119,23 +119,6 @@ namespace PLEXIL
     : GetValueImpl<String>(),
     m_value(value),
     m_known(true)
-  {
-  }
-
-  /**
-   * @brief Destructor.
-   */
-  template <typename T>
-  Constant<T>::~Constant()
-  {
-  }
-
-  Constant<String>::~Constant()
-  {
-  }
-
-  template <typename T>
-  Constant<ArrayImpl<T> >::~Constant()
   {
   }
 
