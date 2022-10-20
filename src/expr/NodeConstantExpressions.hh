8157898f33997136b6a850057673420fd29ff5c1
Chuck Fry <Chuck.Fry@NASA.GOV>
2022-10-20 15:12:29 -0700

    NodeConstantExpressions.hh: Remove now-redundant include files CommandHandle.hh, NodeConstants.hh

--- src/expr/NodeConstantExpressions.hh
+++ src/expr/NodeConstantExpressions.hh
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2021, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2022, Universities Space Research Association (USRA).
  *  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,9 +28,6 @@
 #define PLEXIL_NODE_CONSTANT_EXPRESSIONS_HH
 
 #include "Constant.hh"
-
-#include "CommandHandle.hh"
-#include "NodeConstants.hh"
 
 namespace PLEXIL
 {
