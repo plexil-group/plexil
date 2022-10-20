5f8919e0b9ae15bed8e61503c71cc4601e4ae1e7
Chuck Fry <Chuck.Fry@NASA.GOV>
2022-10-20 15:34:56 -0700

    NodeTimepointValue.cc: Add #include <iostream>

--- src/exec/NodeTimepointValue.cc
+++ src/exec/NodeTimepointValue.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2020, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2022, Universities Space Research Association (USRA).
  *  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,6 +28,7 @@
 
 #include "NodeConnector.hh"
 
+#include <iostream>
 #include <iomanip> // for std::setprecision()
 
 namespace PLEXIL
