2bdaf9de338ee17fd5f35725c0d6eedf8f94124c
Chuck Fry <Chuck.Fry@NASA.GOV>
2022-10-20 17:34:32 -0700

    examples/checkpoint/stringFunctions.cc: Value::printValue() replaced with Value::print()

--- examples/checkpoint/stringFunctions.cc
+++ examples/checkpoint/stringFunctions.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2021, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2022, Universities Space Research Association (USRA).
  *  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -62,7 +62,7 @@ Value toStringFunction(const vector<Value> &args)
     {
       ostringstream s;
       for (Value const &v : args)
-        v.printValue(s);
+        v.print(s);
       return Value(s.str());
     }
   }
