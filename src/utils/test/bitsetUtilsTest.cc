f29a86cf2d826338c474592b7928abedcb941633
Chuck Fry <Chuck.Fry@NASA.GOV>
2022-10-20 18:36:34 -0700

    src/utils/test/bitsetUtilsTest.cc: Add #include <climits>

--- src/utils/test/bitsetUtilsTest.cc
+++ src/utils/test/bitsetUtilsTest.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2006-2020, Universities Space Research Association (USRA).
+/* Copyright (c) 2006-2022, Universities Space Research Association (USRA).
 *  All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
@@ -28,6 +28,8 @@
 #include "TestSupport.hh"
 
 #include "plexil-stdint.h"
+
+#include <climits>
 
 using namespace PLEXIL;
 
